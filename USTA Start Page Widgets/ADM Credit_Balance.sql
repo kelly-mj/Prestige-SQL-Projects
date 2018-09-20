bcb69f3da96ff4988bea494577aeccb747a06b95
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-09-20 09:35:53 -0400

    Edited Credit Balance sql

--- USTA Start Page Widgets/ADM Credit_Balance.sql
+++ USTA Start Page Widgets/ADM Credit_Balance.sql
@@ -1,19 +1,43 @@
-SELECT Distinct
-CONCAT('<a href="student_account_ledger.jsp?studentId=', CAST(SDT.studentId AS CHAR), '">', CAST(SDT.firstName AS CHAR), ' ', CAST(SDT.lastName AS CHAR), '</a>') AS Name,
-(SELECT programmeName FROM Programmes Where programmeId = R.programmeId) as 'PROGRAM',
-ROUND((Select balance From StudentLedger  Where studentId =SAL.studentId AND registrationId = SAL.registrationId AND itemNo = ITEM.itemNo ),2) as 'Balance',
-DATE_FORMAT((Select billingDate From StudentLedger  Where studentId =SAL.studentId AND registrationId = SAL.registrationId AND itemNo = ITEM.itemNo ), "%m /%d /%Y") as 'BILLING_DATE', 
-DATE_FORMAT((Select DATE_ADD(billingDate, INTERVAL 14 DAY) From StudentLedger  Where studentId =SAL.studentId AND registrationId = SAL.registrationId AND itemNo = ITEM.itemNo ), "%m /%d /%Y") as 'DUE DATE'
+-- Created by ?? on ??
+-- Edited by Kelly MJ
+-- Update: 6/27/2018
+   -- Improved formatting; added comments    
 
-From StudentLedger SAL 
-INNER JOIN  Registrations R ON R.registrationId = SAL.registrationId AND R.studentId = SAL.studentId
-INNER JOIN  Students SDT ON SDT.studentId = SAL.studentId
+SELECT Distinct
+    CONCAT('<a href="student_account_ledger.jsp?studentId=', CAST(SDT.studentId AS CHAR), '">', CAST(SDT.firstName AS CHAR), ' ',   CAST(SDT.lastName AS CHAR), '</a>') AS Name
+  , (SELECT programmeName
+     FROM Programmes 
+     WHERE programmeId = R.programmeId) as 'PROGRAM'
+  , ROUND((SELECT balance From StudentLedger 
+           WHERE studentId =SAL.studentId
+           AND registrationId = SAL.registrationId
+           AND itemNo = ITEM.itemNo ), 2) as 'Balance'
+  , DATE_FORMAT((SELECT billingDate 
+                 FROM StudentLedger  
+                 WHERE studentId =SAL.studentId 
+                 AND registrationId = SAL.registrationId 
+                 AND itemNo = ITEM.itemNo ), "%m /%d /%Y") as 'BILLING_DATE'
+  , DATE_FORMAT((Select DATE_ADD(billingDate, INTERVAL 14 DAY) From StudentLedger  Where studentId =SAL.studentId AND registrationId = SAL.registrationId AND itemNo = ITEM.itemNo ), "%m /%d /%Y") as 'DUE DATE'
+
+FROM StudentLedger SAL 
+
+INNER JOIN  Registrations R
+ON R.registrationId = SAL.registrationId
+AND R.studentId = SAL.studentId
+AND R.regStatus = 1
+And R.isActive = 1
+
+INNER JOIN  Students SDT
+ON SDT.studentId = SAL.studentId
+AND S.isActive = 1
+
 INNER JOIN 
-      (
-      SELECT registrationId, MAX(itemNo) 'itemNo'
-      From StudentLedger
-      Where billingDate <= CURDATE() 
-      Group By registrationId
-      ) ITEM ON SAL.registrationId = ITEM.registrationId
-Where SAL.<ADMINID>
-HAVING Balance < 0 
+			( SELECT registrationId, MAX(itemNo) 'itemNo'
+			  FROM StudentLedger
+			  WHERE billingDate <= CURDATE() 
+			  GROUP BY registrationId
+			) ITEM
+ON SAL.registrationId = ITEM.registrationId
+
+WHERE SAL.<ADMINID>
+HAVING Balance < 0
