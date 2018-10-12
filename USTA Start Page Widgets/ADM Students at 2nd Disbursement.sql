92d9b2cf003c2f9a281d850f1bc2ae14de2e1fe2
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-10-12 12:18:11 -0400

    Update USTA Start Page Widget

--- USTA Start Page Widgets/ADM Students at 2nd Disbursement.sql
+++ USTA Start Page Widgets/ADM Students at 2nd Disbursement.sql
@@ -1,5 +1,6 @@
 -- ADM Students at 2nd Disbursement
 -- Written by Zach or Andrew
+-- 10/11/18 Kelly MJ: Added CSR constraint to fix attendance hours
 
 SELECT CONCAT('<a href="admin_view_student.jsp?studentid=', CAST(userZ.SID AS CHAR), '">', First, ' ', Last, '</a>') AS 'Student Name'
     , PRG.programmeName as 'Program Name'
@@ -20,9 +21,25 @@ INNER JOIN (
 		, CLS.subjectId AS Subject_ID
 		, FORMAT(SUM(ATD.duration),2) AS Hours_Attended
 	FROM Attendance ATD
-	INNER JOIN Students SDT ON ATD.studentId = SDT.studentId AND SDT.isActive = 1   -- 1 means active students only
-	INNER JOIN Classes CLS ON ATD.classId = CLS.classId AND CLS.isActive=1
-	WHERE NOT ATD.isActive=0
+	INNER JOIN Students SDT
+		ON ATD.studentId = SDT.studentId
+		AND SDT.isActive = 1   -- 1 means active students only
+	INNER JOIN Registrations R
+		ON R.studentId = SDT.studentId
+	INNER JOIN (SELECT studentId, MAX(startDate) AS maxStartDate FROM Registrations GROUP BY studentId) RR
+		ON RR.studentId = R.studentId AND RR.maxStartDate = R.startDate
+	INNER JOIN Classes CLS
+		ON ATD.classId = CLS.classId
+		AND CLS.isActive=1
+		AND CLS.subjectId IN (SELECT subjectId FROM GroupSubjectReltn GSR, CourseGroups CG
+          WHERE CG.programmeId=R.programmeId AND CG.isActive=1
+                    AND CG.courseGroupId=GSR.courseGroupId AND GSR.isActive=1)
+	INNER JOIN ClassStudentReltn CSR
+		ON CSR.studentId = ATD.studentId
+		AND CSR.classId = CLS.classId
+		AND CSR.isActive = 1
+	WHERE ATD.isActive = 1
+	  AND ATD.attendanceDate >= R.startDate
 	GROUP BY ATD.studentId) AS userZ 
     ON REG.studentId = userZ.SID
 WHERE REG.isActive=1
@@ -36,9 +53,9 @@ AND CASE PRG.programmeID
 -- 2 = Massage
 -- 3 = Instructor
 AND REG.<ADMINID>
-AND userZ.Subject_ID IN (SELECT GSR.subjectId
+/*AND userZ.Subject_ID IN (SELECT GSR.subjectId
 					FROM CourseGroups CGP
 					INNER JOIN GroupSubjectReltn GSR ON CGP.courseGroupId=GSR.courseGroupId AND GSR.isActive=1
-					WHERE REG.programmeId = CGP.programmeId and CGP.isActive=1)
+					WHERE REG.programmeId = CGP.programmeId and CGP.isActive=1)*/
 
 ORDER BY Last
