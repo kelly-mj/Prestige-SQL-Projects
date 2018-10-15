a20183176caf5615104dc1791db84f9509602f9a
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-10-15 12:31:10 -0400

    Update FIN Financial Assessment Needed
    Also deleted dumb folder in USTA queries

--- FRANKLIN Start Page Widgets/FIN Financial Aid Assessment Needed.sql
+++ FRANKLIN Start Page Widgets/FIN Financial Aid Assessment Needed.sql
@@ -1,31 +1,54 @@
 -- FIN Financial Aid Assessment Needed
 -- Written by Andrew
+-- 10/15/18 Kelly MJ: Refactored code, improved calculation for total hours attended
 
-SELECT CONCAT('<a href="admin_view_student.jsp?studentid=', CAST(SDT.studentId AS CHAR), '">', SDT.firstName, ' ', SDT.lastName, '</a>') AS 'Student Name',
-(Select PRG.programmeName From Programmes PRG Where PRG.<ADMINID> AND PRG.programmeId = REG.programmeId AND PRG.isActive=1) AS 'Enrolled Program',
-	CMP.campusName AS 'Campus',
-	CAST(SUM(ATD.duration) AS dec(10)) as 'Hours Attended'
+SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(t1.studentId AS CHAR), '">', t1.studentName, '</a>') AS 'Name'
+	, t1.programmeName 'Enrolled Program',
+	, t1.attHours 'Hours Attended'
+	-- , t1.fieldValue 'Financial Aid Assessment?'
 
-FROM Registrations REG
-INNER JOIN Attendance ATD ON REG.studentId=ATD.studentId AND ATD.isActive=1
-INNER JOIN Students SDT ON REG.studentId = SDT.studentId AND SDT.isActive=1
-INNER JOIN Campuses CMP ON SDT.studentCampus = CMP.campusCode AND CMP.isActive=1
-INNER JOIN Classes CLS ON ATD.classId = CLS.classId AND CLS.isActive=1
-INNER JOIN ProfileFieldValues PVF ON REG.studentID = PVF.userID AND PVF.isActive=1
+FROM (
+	SELECT R.studentId
+		, S.idNumber
+		, CONCAT(S.firstName, ' ', S.lastName) AS studentName
+		, SUM(A.duration) AS attHours
+		, PFV.fieldValue
+		, P.programmeName
 
-WHERE 
-        REG.ADMINID AND REG.isActive=1 AND
-	REG.endDate>=CURDATE() AND
-	REG.enrollmentSemesterId = 4000441 AND
-	ATD.attendanceDate>=REG.startDate AND
-        PVF.fieldName = 'FINANCIAL_AID_ASSESSMENT' AND
-        (PVF.fieldValue IS NULL OR PVF.fieldValue ='FALSE') AND 
- CLS.subjectId IN
-		(SELECT subjectId
-			FROM CourseGroups CGP
-			INNER JOIN GroupSubjectReltn GSR ON CGP.courseGroupId=GSR.courseGroupId AND GSR.isActive=1
-			WHERE REG.programmeId = CGP.programmeId and CGP.isActive=1)
-        	
+	FROM Registrations R
 
-GROUP BY SDT.studentId, CLS.subjectId
-HAVING SUM(ATD.duration) >= 880
+	INNER JOIN (SELECT studentId, MAX(startDate) AS maxDate FROM Registrations GROUP BY studentId) R2
+		ON R2.studentId = R.studentId AND R2.maxDate = R.startDate
+
+	INNER JOIN Programmes P
+		ON P.programmeId = R.programmeId
+
+	INNER JOIN Students S
+		ON S.studentId = R.studentId
+		AND S.isActive = 1
+
+	INNER JOIN ClassStudentReltn CSR
+		ON CSR.studentId = R.studentId
+	    AND CSR.isActive = 1
+
+	INNER JOIN ProfileFieldValues PFV
+		ON PFV.userId = R.studentId
+		AND PFV.fieldName = 'FINANCIAL_AID_ASSESSMENT'
+		-- AND PFV.fieldValue <> 'TRUE'
+
+	INNER JOIN Classes C
+		ON C.classId = CSR.classId
+	    AND C.isActive=1
+	    AND C.startDate <= R.endDate AND C.endDate >= R.startDate
+	    and C.subjectId IN (SELECT subjectId FROM GroupSubjectReltn GSR, CourseGroups CG WHERE CG.programmeId=R.programmeId and CG.isActive=1 and CG.courseGroupId=GSR.courseGroupId and GSR.isActive=1)
+
+	INNER JOIN Attendance A
+		ON A.studentId = S.studentId
+		AND A.attendanceDate >= R.startDate
+		AND A.classId = C.classId
+
+	WHERE R.<ADMINID> AND R.isActive = 1
+	GROUP BY R.studentId
+	) t1
+
+WHERE t1.attHours >= 880
