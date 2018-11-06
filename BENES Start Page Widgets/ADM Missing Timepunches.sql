2733dd4ff8be74ebd72104b5b6cfeced1c90c767
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-11-06 15:16:41 -0500

    Update ADM Missing Timepunches
    Added two date constraints:
    - Exclude records with a date greater than the current date
    - Exclude records with a date before the student's start date

--- BENES Start Page Widgets/ADM Missing Timepunches.sql
+++ BENES Start Page Widgets/ADM Missing Timepunches.sql
@@ -1,19 +1,33 @@
+-- ADM Missing Timepunches
+-- Kelly MJ  |  Sometime before 8/24/18
 -- Kelly MJ 8/24/2018 update: use attendanceClockPunch instead of reasonText
 -- Kelly MJ 9/13/2018 update: excluded attendance records from the current day
 
-SELECT CONCAT('<a href="view_attendance.jsp?semesterid=', CAST(REG.enrollmentSemesterId AS CHAR), '&classid=', CAST(ATD.classId AS CHAR), '&subjectid=', CAST(ATD.subjectId AS CHAR), '&studentid=', CAST(ATD.studentId AS CHAR), '"target="_blank">', CAST(SDT.firstName AS CHAR), ' ', CAST(SDT.lastName AS CHAR), '</a>') AS Name
+SELECT CONCAT('<a target="_blank" href="view_attendance.jsp?semesterid=', CAST(REG.enrollmentSemesterId AS CHAR), '&classid=', CAST(ATD.classId AS CHAR), '&subjectid=', CAST(ATD.subjectId AS CHAR), '&studentid=', CAST(ATD.studentId AS CHAR), '"target="_blank">', CAST(SDT.firstName AS CHAR), ' ', CAST(SDT.lastName AS CHAR), '</a>') AS Name
 	, ATD.duration AS Duration
 	, ATD.attendanceDate AS Attendance_Date
     , ATD.attendanceClockPunch
-FROM
-	Attendance ATD
-	INNER JOIN Registrations REG ON ATD.studentId = REG.studentId
-	INNER JOIN Students SDT ON ATD.studentId = SDT.studentId
-WHERE
-	ATD.<ADMINID> AND NOT ATD.isActive = 0 AND
-	YEAR(ATD.attendanceDate) > 2017 AND
-	ATD.attendanceDate <> CURDATE()
-	ATD.duration = 0 AND ATD.present = 1
+
+FROM Attendance ATD
+
+INNER JOIN Registrations REG
+	ON ATD.studentId = REG.studentId
+
+INNER JOIN (SELECT studentId, MAX(startDate) as maxStart FROM Registrations GROUP BY studentId) RR
+	ON RR.studentId = REG.studentId
+	AND RR.maxStart = REG.startDate
+
+INNER JOIN Students SDT
+	ON ATD.studentId = SDT.studentId
+
+WHERE ATD.<ADMINID>
+	AND NOT ATD.isActive = 0
+	AND YEAR(ATD.attendanceDate) > 2017
+	AND ATD.attendanceDate < CURDATE()
+	AND ATD.attendanceDate >= RR.maxStart
+	AND ATD.duration = 0
+	AND ATD.present = 1
+
 GROUP BY ATD.attendanceId
 ORDER BY Attendance_Date DESC, SDT.lastName 
 LIMIT 10000
