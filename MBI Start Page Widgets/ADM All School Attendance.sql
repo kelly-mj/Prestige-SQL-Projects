d3531cb101d0074389c11ab70157f6804b76d6ed
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-12-10 11:35:46 -0500

    Update ADM All School Attendance.sql

--- MBI Start Page Widgets/ADM All School Attendance.sql
+++ MBI Start Page Widgets/ADM All School Attendance.sql
@@ -1,32 +1,66 @@
-SELECT
-	SDT.idNumber,
-	CONVERT(CONCAT("<a target='_blank' href='admin_view_student_attendance_record.jsp?studentid=", SDT.studentId, "'>", SDT.firstName, ' ', SDT.lastName, "</a>"), CHAR CHARACTER SET utf8) AS Name,
-	CLS.className,
-	IF(IDT.totalPunch % 2 = 1,
-		"Clocked In",
-		IF(IDT.totalPunch % 2 = 0,
-			"Clocked Out",
-			IF(SDT.isactive = 12,
-				'Leave of Absence',
-				'Absent'
-			)
-		)
-	) AS 'Status',
-	IDT.lastPunch AS 'Last Time Clocked'
+-- MBI ADM All School Attendance
+-- Kelly MJ  |  12/10/18
 
-FROM Students SDT
-JOIN ClassStudentReltn CSR ON SDT.studentId = CSR.studentId AND CSR.isActive = 1
-JOIN Classes CLS ON CSR.classId = CLS.classId AND CLS.isActive = 1
-LEFT OUTER JOIN
-	(SELECT
-		idNumber,
-		userId,
-		DATE_FORMAT(MAX(punchTime), '%m-%d @ %l:%i') AS lastPunch,
-		COUNT(userId) AS totalPunch
+SELECT t1.studentId
+	, t1.name
+	, CASE
+		WHEN t1.hours <= 500 THEN TH.className
+		WHEN t1.hours BETWEEN 500 AND 600 THEN CONCAT(TH.className, ' /</br>', IC.className)
+        ELSE IC.className
+	  END AS Class
+    , CP.clockedStatus
+    , CP.lastPunch
+
+FROM (
+    SELECT S.studentId
+		, S.idNumber
+		, CONCAT('<a target="_blank" href="https://mbi.orbund.com/einstein-freshair/admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', S.lastName, ', ', S.firstName, '</a>') AS name
+		, SUM(A.duration) AS hours
+		, R.startDate
+
+	FROM Students S
+
+	INNER JOIN (
+		SELECT studentId, MAX(startDate) AS maxDate FROM Registrations R WHERE isActive = 1 GROUP BY studentId ) AS RR
+		ON RR.studentId = S.studentId
+
+	INNER JOIN Registrations R
+		ON R.studentId = S.studentId 
+		AND R.startDate = RR.maxDate
+		
+	LEFT JOIN Attendance A
+		ON A.studentId = S.studentId
+		AND A.isActive = 1
+		AND A.classId IN (SELECT classId FROM Classes WHERE className NOT LIKE '%intern%' AND className NOT LIKE '%ic%')
+
+	WHERE S.isActive = 1
+		AND S.<ADMINID>
+		
+	GROUP BY S.studentId ) t1
+
+LEFT JOIN (
+	SELECT C.className, CSR.studentId
+    FROM ClassStudentReltn CSR
+    INNER JOIN Classes C
+		ON C.classId = CSR.classId
+        AND C.className NOT LIKE '%intern%' AND C.className NOT LIKE '%ic%'
+        AND C.isActive = 1 AND CSR.isActive = 1 ) TH
+	ON TH.studentId = t1.studentId
+    
+LEFT JOIN (
+	SELECT C.className, CSR.studentId
+    FROM ClassStudentReltn CSR
+    INNER JOIN Classes C
+		ON C.classId = CSR.classId
+        AND C.className LIKE '%intern%' AND C.className LIKE '%ic%'
+        AND C.isActive = 1 AND CSR.isActive = 1 ) IC
+	ON IC.studentId = t1.studentId
+    
+INNER JOIN (
+	SELECT userId
+		, DATE_FORMAT(MAX(punchTime), '%a %m/%d/%y</br>%h:%i %p') AS lastPunch
+        , CONCAT(IF(clockedStatus%2=1, 'Clocked In', 'Clocked Out')) AS clockedStatus
 	FROM ClockPunches
-	WHERE <ADMINID> AND isActive=1 AND
-	CURDATE() = DATE(punchTime)
-	GROUP BY userId) AS IDT ON SDT.studentId = IDT.userId
-WHERE SDT.<ADMINID> AND SDT.isActive = 1  OR
-		SDT.<ADMINID> AND SDT.isActive = 12
-Order By CLS.className, IDT.lastPunch DESC
+    WHERE userId IN (SELECT studentId FROM Students WHERE isActive = 1)
+    GROUP BY userId ) CP
+    ON CP.userId = t1.studentId
