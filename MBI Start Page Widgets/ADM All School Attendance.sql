19abbf6dac679e025f3036f25ed555c3308e283a
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-12-10 11:34:48 -0500

    Entered old version

--- MBI Start Page Widgets/ADM All School Attendance.sql
+++ MBI Start Page Widgets/ADM All School Attendance.sql
@@ -1,23 +1,32 @@
-SELECT CONCAT('<a target="_blank" href="https://mbi.orbund.com/einstein-freshair/admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', S.lastName, ', ', S.firstName, '</a>') AS name
-    , SUM(A.duration) AS totalHours
-	, R.startDate
+SELECT
+	SDT.idNumber,
+	CONVERT(CONCAT("<a target='_blank' href='admin_view_student_attendance_record.jsp?studentid=", SDT.studentId, "'>", SDT.firstName, ' ', SDT.lastName, "</a>"), CHAR CHARACTER SET utf8) AS Name,
+	CLS.className,
+	IF(IDT.totalPunch % 2 = 1,
+		"Clocked In",
+		IF(IDT.totalPunch % 2 = 0,
+			"Clocked Out",
+			IF(SDT.isactive = 12,
+				'Leave of Absence',
+				'Absent'
+			)
+		)
+	) AS 'Status',
+	IDT.lastPunch AS 'Last Time Clocked'
 
-FROM Students S
-
-INNER JOIN (
-	SELECT studentId, MAX(startDate) AS maxDate FROM Registrations R WHERE isActive = 1 GROUP BY studentId ) AS RR
-    ON RR.studentId = S.studentId
-
-INNER JOIN Registrations R
-	ON R.studentId = S.studentId 
-    AND R.startDate = RR.maxDate
-    
-LEFT JOIN Attendance A
-	ON A.studentId = S.studentId
-    AND A.isActive = 1
-    AND A.classId IN (SELECT classId FROM Classes WHERE className NOT LIKE '%intern%' AND className NOT LIKE '%ic%')
-
-WHERE S.isActive = 1
-	AND S.<ADMINID>
-    
-GROUP BY S.studentId
+FROM Students SDT
+JOIN ClassStudentReltn CSR ON SDT.studentId = CSR.studentId AND CSR.isActive = 1
+JOIN Classes CLS ON CSR.classId = CLS.classId AND CLS.isActive = 1
+LEFT OUTER JOIN
+	(SELECT
+		idNumber,
+		userId,
+		DATE_FORMAT(MAX(punchTime), '%m-%d @ %l:%i') AS lastPunch,
+		COUNT(userId) AS totalPunch
+	FROM ClockPunches
+	WHERE <ADMINID> AND isActive=1 AND
+	CURDATE() = DATE(punchTime)
+	GROUP BY userId) AS IDT ON SDT.studentId = IDT.userId
+WHERE SDT.<ADMINID> AND SDT.isActive = 1  OR
+		SDT.<ADMINID> AND SDT.isActive = 12
+Order By CLS.className, IDT.lastPunch DESC
