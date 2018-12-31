3b9476c34ad5a386562dc57fbd5430126dadbc04
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-12-31 09:13:44 -0500

    Update ADM Student SAP.sql

--- SDB Start Page Widgets/ADM Student SAP.sql
+++ SDB Start Page Widgets/ADM Student SAP.sql
@@ -1,34 +1,87 @@
 -- SBD ADM Student SAP
+-- Kelly MJ  |  12/31/2018
 
-SELECT CONCAT('<a href="admin_view_student.jsp?studentid=', CAST(SDT.studentId AS CHAR), '">', SDT.firstName, ' ', SDT.lastName, '</a>') AS 'Student Name'
+/*
+ *	Students who have accumulated over 400 scheduled hours
+ */
+
+SELECT '<strong>Over 400 Scheduled Hours</strong>' AS 'Name'
+	, NULL 'Program'
+	, NULL 'Sch.<br>Hours'
+	, NULL 'Actual<br>Hours'
+	, NUll 'Att %'
+	, NULL 'Program<br>Completion'
+	, NULL 'Program<br>Hours'
+
+UNION (
+SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(SDT.studentId AS CHAR), '">', SDT.lastName, ', ', SDT.firstName, '</a>') AS 'Name'
 	, PRG.programmeName AS Program
-	, CMP.campusName AS 'Campus'
-	, Concat( '<div align="center">', CAST(SUM(CLS.lessonDuration) AS dec(10)) ,'</div>') As 'Scheduled Hours'
+	, SCH.fieldValue AS 'Sch. Hours'
 	, CAST(SUM(ATD.duration) AS dec(10)) as 'Actual Hours'
-	, CONCAT( '<div align="center">', ROUND(SUM(ATD.duration)/SUM(CLS.lessonDuration)*100,1),'%', '</div>') As 'Attendance Percentage'
-	, CONCAT( '<div align="center">', ROUND(SUM(ATD.duration)/(PRG.minClockHours)*100,1),'%', '</div>') As 'Program Completion'
-	, PRG.minClockHours as 'Total Hours'
+	, CONCAT( '<div align="center">', ROUND(SUM(100*ATD.duration)/SCH.fieldValue, 0),'%', '</div>') As 'Att %'
+	, CONCAT( '<div align="center">', ROUND(SUM(ATD.duration)/(PRG.minClockHours)*100, 0),'%', '</div>') As 'Program<br>Completion'
+	, PRG.minClockHours as 'Program<br>Hours'
 
 FROM Registrations REG
 
 INNER JOIN Attendance ATD
 	ON REG.studentId=ATD.studentId AND ATD.isActive=1
 
-INNER JOIN Classes CLS
-	ON ATD.classId = CLS.classId AND CLS.isActive=1
+INNER JOIN ProfileFieldValues SCH
+	ON SCH.userId = REG.studentId
+	AND SCH.fieldName = 'PROGRAM_HOURS_SCHEDULED'
+	AND SCH.isActive = 1
 
 INNER JOIN Students SDT
 	ON REG.studentId = SDT.studentId AND SDT.isActive=1
-
-INNER JOIN Campuses CMP
-	ON SDT.studentCampus = CMP.campusCode AND CMP.isActive=1
 
 INNER JOIN Programmes PRG
 	ON PRG.programmeId = REG.programmeId AND PRG.isActive=1
 
 WHERE REG.<ADMINID> AND REG.isActive=1
 AND REG.enrollmentSemesterId = 4000441
-AND REG.endDate>=CURDATE() 
+AND REG.endDate>=CURDATE()
+AND SCH.fieldValue >= 400
+AND SCH.fieldValue < 800
 
 GROUP BY SDT.studentId
-ORDER BY CMP.campusName, SDT.lastName, PRG.programmeName ASC
+ORDER BY SDT.lastName, PRG.programmeName ASC )
+
+/*
+ *	Students who have acculmulated over 800 scheduled hours
+ */
+UNION
+SELECT '<strong>Over 800 Scheduled Hours</strong>' AS 'Name', NULL, NULL, NULL, NULL, NULL, NULL
+
+UNION (
+SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(SDT.studentId AS CHAR), '">', SDT.lastName, ', ', SDT.firstName, '</a>') AS 'Name'
+	, PRG.programmeName AS Program
+	, SCH.fieldValue AS 'Sch. Hours'
+	, CAST(SUM(ATD.duration) AS dec(10)) as 'Actual Hours'
+	, CONCAT( '<div align="center">', ROUND(SUM(100*ATD.duration)/SCH.fieldValue, 0),'%', '</div>') As 'Att %'
+	, CONCAT( '<div align="center">', ROUND(SUM(ATD.duration)/(PRG.minClockHours)*100, 0),'%', '</div>') As 'Program<br>Completion'
+	, PRG.minClockHours as 'Program<br>Hours'
+
+FROM Registrations REG
+
+INNER JOIN Attendance ATD
+	ON REG.studentId=ATD.studentId AND ATD.isActive=1
+
+INNER JOIN ProfileFieldValues SCH
+	ON SCH.userId = REG.studentId
+	AND SCH.fieldName = 'PROGRAM_HOURS_SCHEDULED'
+	AND SCH.isActive = 1
+
+INNER JOIN Students SDT
+	ON REG.studentId = SDT.studentId AND SDT.isActive=1
+
+INNER JOIN Programmes PRG
+	ON PRG.programmeId = REG.programmeId AND PRG.isActive=1
+
+WHERE REG.<ADMINID> AND REG.isActive=1
+AND REG.enrollmentSemesterId = 4000441
+AND REG.endDate>=CURDATE()
+AND SCH.fieldValue >= 400
+
+GROUP BY SDT.studentId
+ORDER BY SDT.lastName, PRG.programmeName ASC )
