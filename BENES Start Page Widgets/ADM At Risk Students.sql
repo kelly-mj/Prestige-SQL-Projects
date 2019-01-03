489cffaab14b748e88689d636ca988caf420bb00
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-01-03 13:02:31 -0500

    Update ADM At Risk Students.sql
    Corrected calculation for consecutive days missed

--- BENES Start Page Widgets/ADM At Risk Students.sql
+++ BENES Start Page Widgets/ADM At Risk Students.sql
@@ -1,16 +1,22 @@
 -- BENES ADM At Risk Students
 -- Written by Andrew
+-- Kelly MJ 1/3/19: Added case for when lda is in a separate year than the current date
 
 SELECT Name AS 'Student'
 ,ClassName AS Class
 , CONCAT('<div align="center">', lda,'</div>') AS 'Last Day Attended'
 , CASE WHEN DifD >= 14 THEN CONCAT('<div align="center">','<font color="red">', Difd, '</font>','</div>') ELSE CONCAT('<div align="center">', Difd, '</div>')  END AS 'Consecutive Days Absent'
+
 FROM
 (
 SELECT Name,Classname, lda, ((DATEDIFF(CURRENT_DATE, lda)) -
-((WEEK(CURRENT_DATE) - WEEK(lda)) * 2) -
+( 2 * IF(YEAR(CURDATE()) <= YEAR(lda), (WEEK(CURRENT_DATE) - WEEK(lda)), 52*(YEAR(CURDATE()) - YEAR(lda) - 1) + 52 - WEEK(lda) + WEEK(CURDATE())) ) -
 (case when weekday(CURRENT_DATE) = 6 then 1 else 0 end) -
 (case when weekday(lda) = 5 then 1 else 0 end)) as DifD
+, DATEDIFF(CURRENT_DATE, lda) as date_diff
+, (WEEK(CURRENT_DATE) - WEEK(lda)) * 2 as week_diff
+, case when weekday(CURRENT_DATE) = 6 then 1 else 0 end AS isSun
+, case when weekday(lda) = 5 then 1 else 0 end AS isSat
 FROM(
 SELECT Distinct A.studentID,C.ClassName, CONCAT('<a href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', S.firstName, ' ', S.lastName, '</a>') AS Name, MAX(A.attendanceDate) as lda
 FROM Attendance A
