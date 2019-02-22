dc01cdbafb0c367d8e153ef250b0847b4f0dad4c
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-02-22 17:17:39 -0500

    BENES Missing Timepunches and Attendance Record Errors

---
+++ Start Page Widgets BENES/ADM Missing Timepunches - Missing Clock Outs.sql
@@ -0,0 +1,50 @@
+-- ADM Missing Timepunches - Missing Clock Outs
+-- Kelly MJ  |  2/22/2019
+
+SELECT DATE_FORMAT(CP.punchDate, '%m/%d/%Y') 'Attendance Date'
+	, CONCAT('<a target="_blank" href="view_attendance.jsp?semesterid=', CAST(R.enrollmentSemesterId AS CHAR), '&classid=', CAST(CSR.classId AS CHAR), '&subjectid=', CAST(A.subjectId AS CHAR), '&studentid=', CAST(S.studentId AS CHAR), '">', S.firstName, ' ', S.lastName, '</a>') AS 'Student Name'
+	, C.className
+	, CP.punchTimes
+	, R.registrationId, A.attendanceId, CP.clockPunchId, CSR.classStudentReltnId, C.classId
+
+FROM Students S
+
+INNER JOIN ( SELECT studentId, MAX(registrationId) maxReg FROM Registrations WHERE isActive = 1 GROUP BY studentId ) RR
+	ON RR.studentId = S.studentId
+
+INNER JOIN Registrations R
+	ON R.studentId = S.studentId AND R.registrationId = RR.maxReg
+
+LEFT JOIN (
+	SELECT C.userId, C.punchDate
+		, COUNT(C.clockPunchId) AS punchCount
+		, MAX(C.clockPunchId) AS clockPunchId
+		, GROUP_CONCAT(C.punchTime ORDER BY C.clockPunchId SEPARATOR '; ') AS punchTimes
+	FROM (SELECT userId, DATE(punchTime) as punchDate, clockPunchId
+		  	, CONCAT(DATE_FORMAT(punchTime, '%l:%i %p')) AS punchTime
+	      FROM ClockPunches
+		  WHERE isActive > 0 ) C
+		  GROUP BY C.userId, C.punchDate
+	  ) CP ON CP.userId = S.studentId
+
+INNER JOIN Attendance A
+	ON A.studentId = S.studentId AND A.attendanceDate = CP.punchDate
+
+INNER JOIN ClassStudentReltn CSR
+	ON CSR.studentId = S.studentId AND CSR.classId = A.classId
+
+INNER JOIN Classes C
+	ON C.classId = CSR.classId
+
+WHERE S.<ADMINID>
+	AND S.isActive IN (1, 12)
+	AND CP.punchCount % 2 = 1
+	AND CP.punchDate < CURDATE()
+	AND CP.punchDate >= DATE_FORMAT(CURDATE(), '%Y-01-01')
+	AND A.isActive = 1
+	AND CSR.isActive = 1
+	AND C.isActive = 1
+	AND (LENGTH(A.attendanceClockPunch) < (10*CP.punchCount + 5))
+
+ORDER BY CP.punchDate, S.lastName
+LIMIT 1000
--- Start Page Widgets BENES/ADM Missing Timepunches.sql
+++ Start Page Widgets BENES/ADM Missing Timepunches.sql
@@ -3,31 +3,48 @@
 -- Kelly MJ 8/24/2018 update: use attendanceClockPunch instead of reasonText
 -- Kelly MJ 9/13/2018 update: excluded attendance records from the current day
 
-SELECT CONCAT('<a target="_blank" href="view_attendance.jsp?semesterid=', CAST(REG.enrollmentSemesterId AS CHAR), '&classid=', CAST(ATD.classId AS CHAR), '&subjectid=', CAST(ATD.subjectId AS CHAR), '&studentid=', CAST(ATD.studentId AS CHAR), '"target="_blank">', CAST(SDT.firstName AS CHAR), ' ', CAST(SDT.lastName AS CHAR), '</a>') AS Name
-	, ATD.duration AS Duration
-	, ATD.attendanceDate AS Attendance_Date
-    , ATD.attendanceClockPunch
+SELECT CONCAT('<a target="_blank" href="admin_view_student_attendance_record.jsp?studentid=', CAST(S.studentId AS CHAR), '">', S.firstName, ' ', S.lastName, '</a>') AS 'Student Name'
+	-- , A.attendanceDate
+	-- , A.present
+	-- , A.duration
+	-- , A.attendanceClockPunch
+	, CP.punchDate
+	, CP.punchTimes
+	, CP.punchCount
+	-- , IF(LENGTH(A.attendanceClockPunch) < (10*CP.punchCount + 5), 'N', 'Y') AS 'Corrected?'
 
-FROM Attendance ATD
+FROM Students S
 
-INNER JOIN Registrations REG
-	ON ATD.studentId = REG.studentId
+INNER JOIN ( SELECT studentId, MAX(registrationId) maxReg FROM Registrations WHERE isActive = 1 GROUP BY studentId ) RR
 
-INNER JOIN (SELECT studentId, MAX(startDate) as maxStart FROM Registrations GROUP BY studentId) RR
-	ON RR.studentId = REG.studentId
-	AND RR.maxStart = REG.startDate
+INNER JOIN Registrations R ON R.studentId = S.studentId
+	AND R.registrationId = RR.maxReg
 
-INNER JOIN Students SDT
-	ON ATD.studentId = SDT.studentId
+/*
+INNER JOIN Attendance A ON A.studentId = S.studentId -- AND A.attendanceDate = CP.punchDate
+	AND ( (A.present <> 1 AND A.attendanceClockPunch > "1") OR (A.present = 1 AND A.duration = 0) )
+	AND A.attendanceDate >= '2019-01-01' AND A.attendanceDate < CURDATE()
+*/
 
-WHERE ATD.<ADMINID>
-	AND NOT ATD.isActive = 0
-	AND YEAR(ATD.attendanceDate) > 2017
-	AND ATD.attendanceDate < CURDATE()
-	AND ATD.attendanceDate >= RR.maxStart
-	AND ATD.duration = 0
-	AND ATD.present = 1
+LEFT JOIN (
+	SELECT C.userId, C.punchDate, COUNT(C.clockPunchId) AS punchCount, GROUP_CONCAT(C.punchTime ORDER BY C.clockPunchId SEPARATOR ' ') AS punchTimes
+	FROM (
+		  SELECT userId, DATE(punchTime) as punchDate, clockPunchId
+		  	, CONCAT(IF(clockedStatus%2 = 1, DATE_FORMAT(punchTime, '%l:%i %p -'), DATE_FORMAT(punchTime, '%l:%i %p; '))) AS punchTime
+	      FROM ClockPunches
+		  WHERE isActive > 0 ) C
+		  GROUP BY C.userId, C.punchDate
+	  ) CP ON CP.userId = S.studentId
+	AND CP.punchDate >= DATE_FORMAT(CURDATE(), '%Y-01-01')
+	AND (CP.punchCount % 2 = 1)
 
-GROUP BY ATD.attendanceId
-ORDER BY Attendance_Date DESC, SDT.lastName 
-LIMIT 10000
+WHERE S.<ADMINID>
+	AND S.isActive IN (1, 12)
+	AND CP.punchDate < CURDATE()
+	AND CP.punchDate >= '2019-01-01'
+
+ORDER BY
+	CP.punchDate
+	-- A.attendanceDate
+	, S.lastName
+LIMIT 1000
