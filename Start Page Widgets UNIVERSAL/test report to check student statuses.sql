a519dd117188b194ab37fbe22176193a7f6df9bb
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-02-25 10:21:57 -0500

    TSPA New Start Students, Chnages to test code

--- Start Page Widgets UNIVERSAL/test report to check student statuses.sql
+++ Start Page Widgets UNIVERSAL/test report to check student statuses.sql
@@ -1,9 +1,12 @@
 -- USE TEST CODE TO CHECK STUDENT STATUSES
 
-SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', CAST(S.lastName AS CHAR), ', ', CAST(S.firstName AS CHAR), '</a>') AS Name -- student name
+SELECT S.studentCampus
+, CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', CAST(S.lastName AS CHAR), ', ', CAST(S.firstName AS CHAR), '</a>') AS Name -- student name
 , S.isActive
 , R.regStatus
-, CONCAT(R.startDate, ' - ', R.endDate), R.graduationDate
+, R.startDate
+, R.endDate
+, R.graduationDate
 
 FROM Students S
 INNER JOIN ( SELECT studentId, MAX(startDate) AS maxDate FROM Registrations WHERE isActive = 1 GROUP BY studentId ) RR
