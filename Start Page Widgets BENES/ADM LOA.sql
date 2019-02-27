40ceb5cee1f6d702c31147bf4c2dcd0ad2b9e1cd
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-02-27 14:12:06 -0500

    Update ADM LOA.sql
    Removed "LOA2" INNER JOIN from PastLOA - allowed query to count number of days from all past LOAs

--- Start Page Widgets BENES/ADM LOA.sql
+++ Start Page Widgets BENES/ADM LOA.sql
@@ -1,19 +1,19 @@
 -- LOA Start Page Query
 -- Update: Kelly MJ  |  07/30/2018
     -- Added 'Actual Hours' column
-    
+
 SELECT CASE WHEN t1.studentCampus = 34652 THEN 'New Port Richey'
 			WHEN t1.studentCampus = 34601 THEN 'Spring Hill'
 			WHEN t1.studentCampus = 34606 THEN 'Brookesville'
 		END AS Campus
 
 , Name
-	
+
 ,  CASE WHEN (CurrentDiff +  PAST) >= 170 THEN CONCAT('<font color="white">','<div style="background-color:#ff0000; width: 100%; height:100%; margin:-3px -3px -3px -5px; padding:4px 4px 2px 4px"> ' ,LeaveDate,'</div>','</font>')
        ELSE LeaveDate
        END AS 'Leave Date'
 
-,   CASE WHEN (CurrentDiff +  PAST) >= 170 THEN CONCAT('<font color="white">','<div style="background-color:#ff0000; width: 100%; height:100%; margin:-3px -3px -3px -5px; padding:4px 4px 2px 4px">',expectedReturnDate,'</div>','</font>') 
+,   CASE WHEN (CurrentDiff +  PAST) >= 170 THEN CONCAT('<font color="white">','<div style="background-color:#ff0000; width: 100%; height:100%; margin:-3px -3px -3px -5px; padding:4px 4px 2px 4px">',expectedReturnDate,'</div>','</font>')
 	   WHEN expectedReturnDate < CURDATE() THEN CONCAT('<font color="black">','<div style="background-color:#ff0000; width: 100%; height:100%; margin:-3px -3px -3px -5px; padding:4px 4px 2px 4px">' , expectedReturnDate,'</div>','</font>')
 	   ELSE expectedReturnDate
        END AS 'Exp. Return'
@@ -27,8 +27,7 @@ SELECT CASE WHEN t1.studentCampus = 34652 THEN 'New Port Richey'
 	   END AS 'Program'
 	,  TRUNCATE(SUM(ATT.duration), 0) AS 'Actual Hours'
 
-
-FROM 
+FROM
 
 	  (SELECT DISTINCT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=',CAST(CurrentLOA.StudentID AS CHAR), '">',firstName, ' ', lastName, '</a>') AS Name
 	  	   , CurrentLOA.StudentID
@@ -38,7 +37,7 @@ FROM
 		   , expectedReturnDate
            , CurrentDiff
            , CurrentLOA.ProgrammeName
-           ,  CASE WHEN SUM(PastSum) IS NULL THEN 0 ELSE SUM(PastSum) END AS Past
+           , CASE WHEN SUM(PastSum) IS NULL THEN 0 ELSE SUM(PastSum) END AS Past
 	  FROM
       (SELECT STD.StudentID
 			, CONCAT(UCASE(SUBSTRING(Firstname, 1, 1)),LCASE(SUBSTRING(Firstname, 2))) AS FirstName
@@ -51,31 +50,30 @@ FROM
 			, DATEDIFF(CURDATE(), LeaveDate) AS CurrentDiff
 			, ProgrammeName
 		FROM Students STD
-		INNER JOIN LeavesOfAbsence LOA 
+		INNER JOIN LeavesOfAbsence LOA
 				ON LOA.studentId = STD.studentId AND LOA.isActive = 1
-		INNER JOIN Registrations REG 
-				ON REG.StudentID = LOA.studentID 
-				AND REG.isactive = 1 
+		INNER JOIN Registrations REG
+				ON REG.StudentID = LOA.studentID
+				AND REG.isactive = 1
 				AND (REG.regstatus = 1 or REG.regstatus = 12)
-		INNER JOIN Programmes PRG 
+		INNER JOIN Programmes PRG
 				ON PRG.ProgrammeID = REG.programmeID
 		WHERE ReturnDate IS NULL AND STD.<ADMINID>
         GROUP BY STD.StudentID, LastName) AS CurrentLOA
 
-LEFT JOIN 
+LEFT JOIN
 	   (SELECT STD.StudentID
              , DATEDIFF(ReturnDate, LeaveDate) AS PastSum
 	         , ProgrammeName
 		FROM Students STD
-		INNER JOIN LeavesOfAbsence LOA 
+		INNER JOIN LeavesOfAbsence LOA
 				ON STD.studentId = LOA.studentId
-		INNER JOIN ( SELECT studentId, MAX(leavesOfAbsenceId) AS maxId FROM LeavesOfAbsence GROUP BY studentId ) LOA2
-				ON LOA2.studentId = LOA.studentId AND LOA2.maxId = LOA.leavesOfAbsenceId
-		INNER JOIN Registrations REG 
+		INNER JOIN Registrations REG
 				ON LOA.studentID = REG.studentID
-		INNER JOIN Programmes PRG 
+		INNER JOIN Programmes PRG
 				ON PRG.ProgrammeID = REG.ProgrammeId
-		GROUP BY LOA.leavesOfAbsenceId) AS PastLOA ON CurrentLOA.StudentID = PastLOA.StudentID 
+        WHERE STD.<ADMINID> AND LOA.isActive = 1
+		GROUP BY LOA.leavesOfAbsenceId) AS PastLOA ON CurrentLOA.StudentID = PastLOA.StudentID
 										AND CurrentLOA.ProgrammeName = PastLOA.ProgrammeName
 GROUP BY CurrentLOA.StudentID) AS t1
 
@@ -99,6 +97,8 @@ LEFT JOIN
 		 GROUP BY S.studentId, A.attendanceDate) AS ATT
 ON ATT.studentId = t1.StudentID
 AND ATT.attendanceDate >= ATT.startDate
+
+WHERE LeaveDate <= CURDATE()
 
 GROUP BY ATT.studentId
 
