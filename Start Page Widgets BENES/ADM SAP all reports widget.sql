aa723afb5deb7f77fac596a6c5203ca37784caaa
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-04-08 14:37:05 -0400

    Update ADM SAP all reports widget.sql
     - un-hardcoded folder ids (needed for disbursement folder url)
     - added actual and scheduled hours as subqueries, not inner joins - query doesn't take forever to load now
     - 'Start Date' links to Disb 1 folder
     - SAP 1 links to Disb 2 folder
     - SAP 2 links to Disb 3 folder
     - SAP 3 links to Disb 4 folder

--- Start Page Widgets BENES/ADM SAP all reports widget.sql
+++ Start Page Widgets BENES/ADM SAP all reports widget.sql
@@ -2,106 +2,101 @@
 -- Kelly MJ | 3/27/19
 -- Displays whether there is a SAP report for SAP Periods 1, 2, 3, 4.
 
--- Last Modifed by Nilima, added one missing field - actualHours on t1
-
 SELECT t1.Name
     , t1.Program
     , FORMAT(t1.programHours, 1) 'Program Hours'
-    , FORMAT(t1.hoursScheduled, 1) 'Hours Scheduled'
-    , FORMAT(t1.actualHours, 1) 'Actual Hours'
-    , IF(t1.fileCount1 IS NOT NULL
-    , CONCAT('<div style="background-color: #bdefaa;">', SAP1url, COALESCE(DATE_FORMAT(t1.dueDate1, '%m/%d/%y'), 'No due date'), '</a></div>')
+    , (SELECT MAX(fieldValue) FROM ProfileFieldValues WHERE userId = t1.studentId AND fieldName = 'PROGRAM_HOURS_SCHEDULED') AS 'Scheduled Hours'
+    , (SELECT MAX(fieldValue) FROM ProfileFieldValues WHERE userId = t1.studentId AND fieldName = 'PROGRAM_HOURS_ATTENDED') AS 'Actual Hours'
+    -- link to Disbursement 1 folder (display student's start date)
+    , CONCAT(disb1url, DATE_FORMAT(t1.startDate, '%m/%d/%y</a>')) AS 'Start Date'
+    -- display whether there is a SAP 1 report in Disbursement 2 folder (due by end of first disbursement)
+    , IF(t1.disb2Files IS NOT NULL
+        , CONCAT('<div style="background-color: #bdefaa;">', disb2url, COALESCE(DATE_FORMAT(t1.dueDate1, '%m/%d/%y'), 'No due date'), '</a></div>')
         , IF(t1.dueDate1 < CURDATE()
-            , CONCAT('<div style="background-color: #ffb7af;">',SAP1url, DATE_FORMAT(t1.dueDate1, '%m/%d/%y'), '</a></div>')
-            , CONCAT('<div>', SAP1url, COALESCE(DATE_FORMAT(t1.dueDate1, 'Due: %m/%d/%y'), 'No due date'), '</a></div>'))) 'Period 1 SAP'
-    , IF(t1.noPP > 1, IF(t1.fileCount2 IS NOT NULL
-        , CONCAT('<div style="background-color: #bdefaa;">', SAP2url, COALESCE(DATE_FORMAT(t1.dueDate2, '%m/%d/%y'), 'No due date'), '</a></div>')
-        , IF(t1.dueDate2 < CURDATE()
-            , CONCAT('<div style="background-color: #ffb7af;">', SAP2url, DATE_FORMAT(t1.dueDate2, '%m/%d/%y'), '</a></div>')
-            , CONCAT('<div>', SAP2url, COALESCE(DATE_FORMAT(t1.dueDate2, 'Due: %m/%d/%y'), 'No due date'), '</a></div>'))), NULL) 'Period 2 SAP'
-    , IF(t1.noPP > 2, IF(t1.fileCount3 IS NOT NULL
-        , CONCAT('<div style="background-color: #bdefaa;">', SAP3url, COALESCE(DATE_FORMAT(t1.dueDate3, '%m/%d/%y'), 'No due date'), '</a></div>')
-        , IF(t1.dueDate3 < CURDATE()
-            , CONCAT('<div style="background-color: #ffb7af;">', SAP3url, DATE_FORMAT(t1.dueDate3, '%m/%d/%y'), '</a></div>')
-            , CONCAT('<div>', SAP3url, COALESCE(DATE_FORMAT(t1.dueDate3, 'Due: %m/%d/%y'), 'No due date'), '</a></div>'))), NULL) 'Period 3 SAP'
-    , IF(t1.noPP > 2, IF(t1.fileCount4 IS NOT NULL
-        , CONCAT('<div style="background-color: #bdefaa;">', SAP4url, COALESCE(DATE_FORMAT(t1.dueDate4, '%m/%d/%y'), 'No due date'), '</a></div>')
-        , IF(t1.dueDate4 < CURDATE()
-            , CONCAT('<div style="background-color: #ffb7af;">', SAP4url, DATE_FORMAT(t1.dueDate4, '%m/%d/%y'), '</a></div>')
-            , CONCAT('<div>', SAP4url, COALESCE(DATE_FORMAT(t1.dueDate4, 'Due: %m/%d/%y'), 'No due date'), '</a></div>'))), NULL) 'Period 4 SAP'
+            , CONCAT('<div style="background-color: #ffb7af;">', disb2url, DATE_FORMAT(t1.dueDate1, '%m/%d/%y'), '</a></div>')
+            , CONCAT('<div>', disb2url, COALESCE(DATE_FORMAT(t1.dueDate1, 'Due: %m/%d/%y'), 'No due date'), '</a></div>'))) 'SAP 1'
+    -- display whether there is a SAP 2 report in Disbursement 3 folder
+    , IF(t1.numPayPeriods > 1
+        , IF(t1.disb3Files IS NOT NULL
+            , CONCAT('<div style="background-color: #bdefaa;">', disb3url, COALESCE(DATE_FORMAT(t1.dueDate2, '%m/%d/%y'), 'No due date'), '</a></div>')
+            , IF(t1.dueDate2 < CURDATE()
+                , CONCAT('<div style="background-color: #ffb7af;">', disb3url, DATE_FORMAT(t1.dueDate2, '%m/%d/%y'), '</a></div>')
+                , CONCAT('<div>', disb3url, COALESCE(DATE_FORMAT(t1.dueDate2, 'Due: %m/%d/%y'), 'No due date'), '</a></div>'))), NULL) 'SAP 2'
+    -- display whether there is a SAP 3 report in Disbursement 4 folder
+    , IF(t1.numPayPeriods > 2
+        , IF(t1.disb4Files IS NOT NULL
+            , CONCAT('<div style="background-color: #bdefaa;">', disb4url, COALESCE(DATE_FORMAT(t1.dueDate3, '%m/%d/%y'), 'No due date'), '</a></div>')
+            , IF(t1.dueDate3 < CURDATE()
+                , CONCAT('<div style="background-color: #ffb7af;">', disb4url, DATE_FORMAT(t1.dueDate3, '%m/%d/%y'), '</a></div>')
+                , CONCAT('<div>', disb4url, COALESCE(DATE_FORMAT(t1.dueDate3, 'Due: %m/%d/%y'), 'No due date'), '</a></div>'))), NULL) 'SAP 3'
 
 FROM (
     SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', S.lastName, ', ', S.firstName, '</a>') AS Name
         , S.studentId
         , S.lastName
+        , R.startDate
         , P.programmeName AS Program
         , P.minClockHours AS programHours
-        , ATT.actualHours
-        , SCH.hoursScheduled
-        , PP.programmeId	-- Matching program in PayPeriod table
-        , PP.ppHours	-- Hours in matching program (1200, 600, 360)
-        , PP.disbPercent	-- How the payments are split up (100%, 50%, 25% per pay period)
-        , PP.numPayPeriods -- How many pay periods there are for the student's program
-        , DISB1.fileCount1
-        , DISB2.fileCount2
-        , DISB3.fileCount3
-        , DISB4.fileCount4
-        , CONCAT('<a target="_blank" href="files_and_documents.jsp?userid=', CAST(S.studentId AS CHAR), '&usertype=1&folderFolderReltnId=116&previousFolderId=0,116">') AS SAP1url
-        , CONCAT('<a target="_blank" href="files_and_documents.jsp?userid=', CAST(S.studentId AS CHAR), '&usertype=1&folderFolderReltnId=117&previousFolderId=0,117">') AS SAP2url
-        , CONCAT('<a target="_blank" href="files_and_documents.jsp?userid=', CAST(S.studentId AS CHAR), '&usertype=1&folderFolderReltnId=118&previousFolderId=0,118">') AS SAP3url
-        , CONCAT('<a target="_blank" href="files_and_documents.jsp?userid=', CAST(S.studentId AS CHAR), '&usertype=1&folderFolderReltnId=119&previousFolderId=0,119">') AS SAP4url
-        , PP1.dueDate1
+        , PP.programmeId	-- Matched program in PayPeriod table
+        , PP.ppHours        -- Total hours in matched program (1200, 600, 360)
+        , PP.numPayPeriods  -- How many pay periods there are in the student's program
+        , DISB2.disb2Files  -- Number of files in each disbursement folder
+        , DISB3.disb3Files
+        , DISB4.disb4Files
+        , PP1.dueDate1      -- Due date for each pay period (also due date for the SAP report)
         , PP2.dueDate2
         , PP3.dueDate3
-        , PP4.dueDate4
-        , CASE PP.ppHours WHEN 1200 THEN 4
-            WHEN 600 THEN IF(PP.disbPercent = 50.0, 2, 1)
-            WHEN 360 THEN IF(PP.disbPercent = 50.0, 2, 1)
-            WHEN 300 THEN 1 ELSE 5 END AS noPP
+        , CONCAT('<a target="_blank" href="files_and_documents.jsp?userid=', CAST(S.studentId AS CHAR), '&usertype=1&folderFolderReltnId='
+                , (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 1'),'&previousFolderId=0">') AS disb1url       -- url for each disbursement folder
+        , CONCAT('<a target="_blank" href="files_and_documents.jsp?userid=', CAST(S.studentId AS CHAR), '&usertype=1&folderFolderReltnId='
+                , (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 2'),'&previousFolderId=0">') AS disb2url
+        , CONCAT('<a target="_blank" href="files_and_documents.jsp?userid=', CAST(S.studentId AS CHAR), '&usertype=1&folderFolderReltnId='
+                , (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 3'),'&previousFolderId=0">') AS disb3url
+        , CONCAT('<a target="_blank" href="files_and_documents.jsp?userid=', CAST(S.studentId AS CHAR), '&usertype=1&folderFolderReltnId='
+                , (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 4'),'&previousFolderId=0">') AS disb4url
 
     FROM Students S
 
-    INNER JOIN (SELECT studentId, MAX(registrationId) AS maxReg FROM Registrations WHERE isActive = 1 GROUP BY studentId) RR
+    -- get details of most recent registration record for each student
+    INNER JOIN (SELECT studentId, MAX(registrationId) AS maxReg FROM Registrations
+            WHERE isActive = 1 AND programmeId NOT IN (SELECT programmeId FROM Programmes WHERE programmeName LIKE '%Pathways%')
+            GROUP BY studentId) RR
         ON RR.studentId = S.studentId
 
     INNER JOIN Registrations R ON R.studentId = S.studentId
-    AND R.registrationId = RR.maxReg
+        AND R.registrationId = RR.maxReg
 
     INNER JOIN Programmes P ON P.programmeId = R.programmeId
 
+    -- get PayPeriod/SAP information for student's current program
     INNER JOIN (SELECT programmeId, MAX(payPeriodHours) ppHours, MAX(payPeriodDisbPercent) disbPercent, COUNT(payPeriodDisbPercent) AS numPayPeriods FROM PayPeriod GROUP BY programmeId) PP
         ON PP.programmeId = R.programmeId
 
-    INNER JOIN (SELECT PFV.userId, PFV.fieldValue AS actualHours FROM ProfileFieldValues PFV WHERE PFV.fieldName = 'PROGRAM_HOURS_ATTENDED') ATT
-        ON ATT.userId = S.studentId
-
-    INNER JOIN (SELECT PFV.userId, PFV.fieldValue AS hoursScheduled FROM ProfileFieldValues PFV WHERE PFV.fieldName = 'PROGRAM_HOURS_SCHEDULED') SCH
-        ON SCH.userId = S.studentId
-
-    LEFT JOIN (SELECT userId, COUNT(folderFileReltnId) AS fileCount1 FROM FolderFileReltn
-				WHERE folderFolderReltnId = (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 1' AND isActive = 1)
-				AND documentTypeId = (SELECT documentTypeId FROM DocumentType WHERE documentType = 'SAP 1' AND isActive = 1)
-				GROUP BY userId) DISB1
-        ON DISB1.userId = S.studentId
-
-    LEFT JOIN (SELECT userId, COUNT(folderFileReltnId) AS fileCount2 FROM FolderFileReltn
-				WHERE folderFolderReltnId = (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 2' AND isActive = 1)
-				AND documentTypeId = (SELECT documentTypeId FROM DocumentType WHERE documentType = 'SAP 2' AND isActive = 1)
-				GROUP BY userId) DISB2
+    -- count SAP 1 reports in Disbursement 2 folder
+    LEFT JOIN (SELECT userId, COUNT(folderFileReltnId) AS disb2Files FROM FolderFileReltn
+                WHERE folderFolderReltnId = (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 2' AND isActive = 1)
+                  AND documentTypeId = (SELECT documentTypeId FROM DocumentType WHERE documentType = 'SAP 1' AND isActive = 1)
+                GROUP BY userId) DISB2
         ON DISB2.userId = S.studentId
 
-    LEFT JOIN (SELECT userId, COUNT(folderFileReltnId) AS fileCount3 FROM FolderFileReltn
-				WHERE folderFolderReltnId = (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 3' AND isActive = 1)
-				AND documentTypeId = (SELECT documentTypeId FROM DocumentType WHERE documentType = 'SAP 3' AND isActive = 1)
-				GROUP BY userId) DISB3
+    -- count SAP 2 reports in Disbursement 3 folder
+    LEFT JOIN (SELECT userId, COUNT(folderFileReltnId) AS disb3Files FROM FolderFileReltn
+                WHERE folderFolderReltnId = (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 3' AND isActive = 1)
+                  AND documentTypeId = (SELECT documentTypeId FROM DocumentType WHERE documentType = 'SAP 2' AND isActive = 1)
+                GROUP BY userId) DISB3
         ON DISB3.userId = S.studentId
 
-    LEFT JOIN (SELECT userId, COUNT(folderFileReltnId) AS fileCount4 FROM FolderFileReltn
-				WHERE folderFolderReltnId = (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 4' AND isActive = 1)
-				AND documentTypeId = (SELECT documentTypeId FROM DocumentType WHERE documentType = 'SAP 4' AND isActive = 1)
-				GROUP BY userId) DISB4
+    -- count SAP 3 reports in Disbursement 4 folder
+    LEFT JOIN (SELECT userId, COUNT(folderFileReltnId) AS disb4Files FROM FolderFileReltn
+                WHERE folderFolderReltnId = (SELECT folderFolderReltnId FROM FolderFolderReltn WHERE folderName = 'Disbursement 4' AND isActive = 1)
+                  AND documentTypeId = (SELECT documentTypeId FROM DocumentType WHERE documentType = 'SAP 3' AND isActive = 1)
+                GROUP BY userId) DISB4
         ON DISB4.userId = S.studentId
 
+    -- get pay period dates for each pay period
+        -- for 1200 hour program, dates should be at 450, 900, 1050, 1200 hours
+        -- for 600 hour programs, dates should be at 300/301, 600 hours (depending on hours split and whether each period's disbursement is 50% or 100% completed hours)
+        -- for 360 hour programs, dates should be at 180 or 360 hours (depending on whether each period's disbursement is 50% or 100%)
     LEFT JOIN (SELECT studentId, payPeriodDate AS dueDate1, registrationId FROM PayPeriodDates WHERE isActive = 1 AND payPeriodNo = 1) PP1
         ON PP1.studentId = S.studentId AND PP1.registrationId = R.registrationId
 
@@ -110,9 +105,6 @@ FROM (
 
     LEFT JOIN (SELECT studentId, payPeriodDate AS dueDate3, registrationId FROM PayPeriodDates WHERE isActive = 1 AND payPeriodNo = 3) PP3
         ON PP3.studentId = S.studentId AND PP3.registrationId = R.registrationId
-
-    LEFT JOIN (SELECT studentId, payPeriodDate AS dueDate4, registrationId FROM PayPeriodDates WHERE isActive = 1 AND payPeriodNo = 4) PP4
-        ON PP4.studentId = S.studentId AND PP4.registrationId = R.registrationId
 
     WHERE S.isActive = 1
         AND S.<ADMINID>
