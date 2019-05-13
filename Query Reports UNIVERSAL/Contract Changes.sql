27ab440254dc43b7523f0ec9df210056bf9fbf48
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-05-13 14:29:24 -0400

    Update Contract Changes.sql
    Deleted name/campus select; added date range select

--- Query Reports UNIVERSAL/Contract Changes.sql
+++ Query Reports UNIVERSAL/Contract Changes.sql
@@ -2,7 +2,7 @@
 -- Kelly MJ 5/13/2019: Displays student records which have had contract changes
 
 SELECT t1.Name
-	, DATE(t1.RA1_dateMax) AS 'Updated'
+	, DATE_FORMAT(t1.RA1_dateMax, '%m/%d/%Y') AS 'Updated'
 	, IF(t1.studentCampus <> t2.studentCampus, (SELECT campusName FROM Campuses WHERE campusCode = t2.studentCampus), '') AS 'Old Campus'
     , IF(t1.studentCampus <> t2.studentCampus, (SELECT campusName FROM Campuses WHERE campusCode = t1.studentCampus), '') AS 'New Campus'
     , IF(t1.startDate <> t2.startDate, DATE_FORMAT(t2.startDate, '%m/%d/%Y'), '') AS 'Old Start Date'
@@ -11,8 +11,8 @@ SELECT t1.Name
     , IF(t1.endDate <> t2.endDate, DATE_FORMAT(t1.endDate, '%m/%d/%Y'), '') AS 'New End Date'
     , IF(t1.programmeId <> t2.programmeId, (SELECT programmeName FROM Programmes WHERE programmeId = t2.programmeId), '') AS 'Old Program'
     , IF(t1.programmeId <> t2.programmeId, (SELECT programmeName FROM Programmes WHERE programmeId = t1.programmeId), '') AS 'New Program'
-    , IF(t1.enrollmentType <> t2.enrollmentType, t2.enrollmentType, '') AS 'Old Enroll. Type'
-    , IF(t1.enrollmentType <> t2.enrollmentType, t1.enrollmentType, '') AS 'New Enroll. Type'
+    , IF(t1.enrollmentType <> t2.enrollmentType, t2.enrollmentType, '') AS 'Old Enroll Type'
+    , IF(t1.enrollmentType <> t2.enrollmentType, t1.enrollmentType, '') AS 'New Enroll Type'
     
 FROM (
 	SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', S.lastName, ', ', S.firstName, '</a>') AS Name
@@ -20,7 +20,7 @@ FROM (
 		, S.lastName
 		, (SELECT MAX(lastUpdateDtTm)
 			FROM Registrations_Audit
-            WHERE registrationId = R.registrationId AND lastUpdateDtTm < RA1_max.RA1_dateMax  AND regStatus IN (1, 12)) AS RA2_dateMax
+            WHERE registrationId = R.registrationId AND lastUpdateDtTm < RA1_max.RA1_dateMax) AS RA2_dateMax
         , RA1.*
         , RA1_max.RA1_dateMax
         
@@ -31,7 +31,7 @@ FROM (
 
 	INNER JOIN (SELECT registrationId, MAX(lastUpdateDtTm) AS RA1_dateMax
 				FROM Registrations_Audit
-                WHERE isActive = 1 AND regStatus IN (1, 12) GROUP BY registrationId) RA1_max
+                WHERE isActive = 1 GROUP BY registrationId) RA1_max
 		ON RA1_max.registrationId = R.registrationId
 
 	INNER JOIN Registrations_Audit RA1
@@ -39,14 +39,7 @@ FROM (
 		AND RA1.lastUpdateDtTm = RA1_max.RA1_dateMax
 	
     WHERE S.isActive = 1
-	  AND CASE '[?Last Name{A-H|A-H|I-P|I-P|Q-Z|Q-Z}]'
-			WHEN 'A-H' THEN SUBSTR(S.lastName, 1, 1) >= 'a' AND SUBSTR(S.lastName, 1, 1) <= 'h'
-            WHEN 'I-P' THEN SUBSTR(S.lastName, 1, 1) >= 'i' AND SUBSTR(S.lastName, 1, 1) <= 'p'
-            WHEN 'Q-Z' THEN SUBSTR(S.lastName, 1, 1) >= 'q' AND SUBSTR(S.lastName, 1, 1) <= 'z' END
-	  AND IF('[?Campus Select (leave blank to select all)]' = ''
-		, RA1.studentCampus <> 'delicious_kielbasa_sausage'
-		, ((RA1.studentCampus = '[?Campus Select (leave blank to select all)]')
-		    OR (RA1.studentCampus = (SELECT MAX(campusCode) FROM Campuses WHERE LOWER(campusName) = LOWER('[?Campus Select (leave blank to select all)]')) )) )
+	  AND DATE(RA1.lastUpdateDtTm) >= IF('[?From Date]' = '', DATE_SUB(CURDATE(), INTERVAL 14 DAY), '[?From Date]')
 	  AND S.<ADMINID>
 	) t1
 
