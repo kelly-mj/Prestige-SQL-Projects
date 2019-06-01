66df686ec04d57a6e44a2a8fbdb5e1218fb7b4d2
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-06-01 18:00:18 -0400

    Create STF_ADM Your School Attendance.sql

---
+++ Start Page Widgets TSPA/STF_ADM Your School Attendance.sql
@@ -0,0 +1,36 @@
+-- [TSPA] STF_ADM Your School Attendance
+
+SELECT
+	SDT.idNumber,
+	CONVERT(CONCAT("<a target='_blank' href='admin_view_student_attendance_record.jsp?studentid=", SDT.studentId, "'>", SDT.firstName, ' ', SDT.lastName, "</a>"), CHAR CHARACTER SET utf8) AS Name,
+	CLS.className,
+	IF(IDT.totalPunch % 2 = 1,
+		"Clocked In",
+		IF(IDT.totalPunch % 2 = 0,
+			"Clocked Out",
+			IF(SDT.studentId = LOA.studentId,
+				'Leave of Absence',
+				'Absent'
+			)
+		)
+	) AS 'Status',
+	IDT.lastPunch AS 'Last Time Clocked'
+
+FROM Students SDT
+
+JOIN ClassStudentReltn CSR ON SDT.studentId = CSR.studentId AND CSR.isActive = 1
+JOIN Classes CLS ON CSR.classId = CLS.classId AND CLS.isActive = 1
+LEFT OUTER JOIN LeavesOfAbsence LOA ON SDT.studentId = LOA.studentId AND LOA.isActive = 1
+LEFT OUTER JOIN
+	(SELECT
+		idNumber,
+		userId,
+		DATE_FORMAT(MAX(punchTime), '%m-%d @ %l:%i') AS lastPunch,
+		COUNT(userId) AS totalPunch
+	FROM ClockPunches
+	WHERE <ADMINID> AND isActive=1 AND
+	CURDATE() = DATE(punchTime)
+	GROUP BY userId) AS IDT ON SDT.studentId = IDT.userId
+
+WHERE SDT.<ADMINID> AND (SDT.isActive = 1 OR SDT.isActive = 12)
+Order By CLS.className, IDT.lastPunch DESC
