a2d50eab8b0b21f49fd2fa288d7fe3a8bd4bccd6
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-06-03 11:01:07 -0400

    Create ADM Campus Average Attendance.sql

---
+++ Start Page Widgets AOHD/ADM Campus Average Attendance.sql
@@ -0,0 +1,21 @@
+-- [AOHD] ADM Campus Average Attendance
+-- Kelly MJ  |  06/03/2019
+
+SELECT C.campusName AS Campus
+    , CONCAT(FORMAT(100*SUM(t1.hoursAtt)/SUM(t1.hoursSch), 2), '%') AS 'Attendance Percentage'
+
+FROM (
+    SELECT S.studentCampus
+        , ATT.fieldValue AS hoursAtt
+        , (SELECT fieldValue FROM ProfileFieldValues WHERE userId = S.studentId AND fieldName = 'PROGRAM_HOURS_SCHEDULED') AS hoursSch
+    FROM Students S
+    INNER JOIN (SELECT userId, fieldValue FROM ProfileFieldValues WHERE fieldName = 'PROGRAM_HOURS_ATTENDED') ATT
+        ON ATT.userId = S.studentId
+    WHERE S.isActive IN (1, 3, 12, 15)
+) t1
+
+INNER JOIN Campuses C ON C.campusCode = t1.studentCampus
+
+WHERE C.<ADMINID>
+
+GROUP BY C.campusCode
