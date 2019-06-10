8a6b6184bf55b9f8b6d4ca34d03d36994cc2c030
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-06-10 14:55:18 -0400

    Create ADM Last 30 Days of Inactive Students.sql

---
+++ Start Page Widgets AOHD/ADM Last 30 Days of Inactive Students.sql
@@ -0,0 +1,18 @@
+-- [AOHD] ADM Last 30 Days of Inactive Students
+-- Kelly MJ  |  6/10/2019
+
+SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', CAST(S.firstName AS CHAR), ' ', CAST(S.lastName AS CHAR), '</a>') AS Name
+	, P.programmeName AS Program
+    , DATE_FORMAT(R.graduationDate, '%m/%d/%Y') AS 'Drop Date'
+    
+FROM Students S
+INNER JOIN (SELECT studentId, MAX(registrationId) AS maxReg FROM Registrations WHERE isActive = 1 GROUP BY studentId) RR
+	ON RR.studentId = S.studentID
+INNER JOIN Registrations R ON R.studentId = S.studentId
+	AND R.registrationId = RR.maxReg
+INNER JOIN Programmes P ON P.programmeId = R.programmeId
+INNER JOIN StatusSequences SS ON SS.statusId = S.isActive
+
+WHERE R.graduationDate > DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+AND SS.statusName IN ('Inactive/Past Student/Dropped', 'Inactive/Past student/Withdrawn')
+AND S.<ADMINID>
