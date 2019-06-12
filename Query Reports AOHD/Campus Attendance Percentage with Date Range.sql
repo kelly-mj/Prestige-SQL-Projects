8360544f766ea4bc9f3944cf7a31736fa3dd65be
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-06-12 11:56:25 -0400

    Update Campus Attendance Percentage with Date Range.sql

--- Query Reports AOHD/Campus Attendance Percentage with Date Range.sql
+++ Query Reports AOHD/Campus Attendance Percentage with Date Range.sql
@@ -1,29 +1,27 @@
 -- [AOHD] Campus Attendance Percentage with Date Range
 -- Kelly MJ  |  06/03/2019
+-- Shows the ratio of overall attended hours to overall scheduled hours. Date range selects active students during the selected window (Active or LOA, not Grad/Withdrew). Separated by campus.
 
-SELECT C.campusName
+SELECT CMP.campusName
     , FORMAT(SUM(t1.hoursAtt), 0) AS 'Total Hours Attended'
-    , FORMAT(SUM(t1.hoursSch), 0) AS 'Total Hours Scheduled'
-    , CONCAT(FORMAT(100*SUM(t1.hoursAtt)/SUM(t1.hoursSch), 2), '%') AS 'Attendance Percentage'
+    , FORMAT(SUM(SCH.hoursSch), 0) AS 'Total Hours Scheduled'
+    , CONCAT(FORMAT(100*SUM(t1.hoursAtt)/SUM(SCH.hoursSch), 2), '%') AS 'Attendance Percentage'
 
 FROM (
-    SELECT S.studentCampus
-		, S.studentId
-        , S.firstName
-        , S.lastName
-        , S.isActive
-        , R.startDate
-        , R.graduationDate
-        , SUM(A.duration) AS hoursAtt
-        , (SELECT fieldValue FROM ProfileFieldValues WHERE userId = S.studentId AND fieldName = 'PROGRAM_HOURS_SCHEDULED') AS hoursSch
-    FROM Registrations R
+    SELECT SUM(A.duration) AS hoursAtt
+        , S.studentId
+        , S.studentCampus
+
+    FROM Students S
     INNER JOIN (SELECT MAX(registrationId) maxReg, studentId FROM Registrations WHERE isActive = 1 GROUP BY studentId) RR
-        ON RR.studentId = R.studentId AND RR.maxReg = R.registrationId
-    INNER JOIN Students S ON S.studentId = R.studentId
+        ON RR.studentId = S.studentId
+    INNER JOIN Registrations R ON R.studentId = S.studentId
+        AND RR.maxReg = R.registrationId
     INNER JOIN Attendance A ON A.studentId = R.studentId
     INNER JOIN ClassStudentReltn CSR ON CSR.studentId = R.studentId
     INNER JOIN Classes C ON C.classId = CSR.classId
-    WHERE( R.graduationDate IS NULL OR R.graduationDate >= '[?From Date]' )
+
+    WHERE ( R.graduationDate IS NULL OR R.graduationDate >= '[?From Date]' )
         AND R.startDate <= '[?To Date]'
         AND A.attendanceDate >= R.startDate
         AND A.isActive = 1
@@ -31,12 +29,12 @@ FROM (
         AND CSR.isActive = 1
         AND C.isActive=1
         AND C.subjectId IN (SELECT subjectId FROM GroupSubjectReltn GSR, CourseGroups CG WHERE CG.programmeId=R.programmeId and CG.isActive=1 and CG.courseGroupId=GSR.courseGroupId and GSR.isActive=1)
-        AND S.isActive <> 14
+        AND S.<ADMINID>
+
     GROUP BY R.studentId
-) t1
+    ) t1
+INNER JOIN (SELECT userId, fieldValue AS hoursSch FROM ProfileFieldValues WHERE fieldName = 'PROGRAM_HOURS_SCHEDULED') SCH
+    ON SCH.userId = t1.studentId
+INNER JOIN Campuses CMP ON CMP.campusCode = t1.studentCampus
 
-INNER JOIN Campuses C ON C.campusCode = t1.studentCampus
-
-WHERE C.<ADMINID>
-
-GROUP BY C.campusId
+GROUP BY t1.studentCampus
