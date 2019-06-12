f05f5f34c20983f461133a0c6818e1e51a0f91d6
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-06-12 11:46:15 -0400

    Update ADM At Risk Students.sql

--- Start Page Widgets TSPA/ADM At Risk Students.sql
+++ Start Page Widgets TSPA/ADM At Risk Students.sql
@@ -1,8 +1,11 @@
+-- [TSPA] ADM, COMP, FIN At Risk Students
+-- Kelly MJ  |  5/22/2019
+-- Displays students who have missed over 65% of their allowed missed hours (program-specific)
+
 SELECT t1.name
 	, t1.Program
-    , t1.hoursAtt AS 'Actual Hours'
+    , FORMAT(t1.hoursAtt, 2) AS 'Actual Hours'
     , t1.hoursSch AS 'Scheduled Hours'
---     , FORMAT(t1.hoursMissed/t1.hoursCutoff, 2)   -- percent of hours cutoff used up
 	, CASE
 		WHEN t1.hoursMissed/t1.hoursCutoff >= 0.90
 			THEN CONCAT('<span style="color: red;"><strong>', CAST(FORMAT(t1.hoursMissed, 2) AS CHAR), '</strong></span>')
@@ -11,29 +14,29 @@ SELECT t1.name
 		ELSE FORMAT(t1.hoursMissed, 2)
 		END AS 'Hours Missed'
     , t1.hoursCutoff AS 'Max Allowed Missed Hours'
-    
+
 FROM (
-    
+
     SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', S.lastName, ', ', S.firstName, '</a>') AS Name
 		, CONCAT(S.lastname, ', ', S.firstName) AS names
 		, P.programmeName AS Program
-        , ATT.fieldValue AS hoursAtt
+        , SUM(A.duration) AS hoursAtt
         , SCH.fieldValue AS hoursSch
-        , SCH.fieldValue - ATT.fieldValue AS hoursMissed
+        , SCH.fieldValue - SUM(A.duration) AS hoursMissed
         , CS.maxStartTime
         , P.minClockHours
         , CASE
 			WHEN P.programmeCode LIKE 'COS1000FT'
 				THEN 55
             WHEN P.programmeCode LIKE 'COS1000PT' AND CS.maxStartTime < 1200
-				THEN 65
+				THEN 70
             WHEN P.programmeCode LIKE 'COS1000PT' AND CS.maxStartTime > 1200
 				THEN 80
 			WHEN P.programmeCode LIKE 'COS1500FT'
 				THEN 77
 			WHEN P.programmeCode LIKE 'COS1500PT'
 				THEN 92
-			WHEN P.programmeCode LIKE 'ESTFT'
+			WHEN P.programmeCode LIKE 'ESTY'
 				THEN 28
 			WHEN P.programmeCode LIKE 'ESTPT'
                 THEN 50
@@ -41,38 +44,40 @@ FROM (
                 THEN 20
 			END AS hoursCutoff
 	, S.studentId as a, R.registrationId as b, P.programmeId as c, CS.maxStartTime AS f
-    
+
 	FROM Students S
 
 	INNER JOIN (SELECT studentId, MAX(registrationId) AS maxReg FROM Registrations WHERE isActive = 1 GROUP BY studentId) RR
 		ON RR.studentId = S.studentId
-		
+
 	INNER JOIN Registrations R
 		ON R.studentId = S.studentId
 		AND R.registrationId = RR.maxReg
-        
+
 	INNER JOIN Programmes P
 		ON P.programmeId = R.programmeId
-        
+
 	LEFT JOIN ( SELECT studentId, classId
 				FROM ClassStudentReltn
                 WHERE isActive = 1 AND status =  0 ) CSR
 		ON CSR.studentId = S.studentId
-    
+
 	LEFT JOIN (SELECT classId, MAX(startTime) AS maxStartTime FROM ClassSchedules GROUP BY classId) CS
 		ON CS.classId = CSR.classId
-		
-	LEFT JOIN ProfileFieldValues ATT
-		ON ATT.userId = S.studentId
-		AND ATT.fieldName = 'PROGRAM_HOURS_ATTENDED'
-		
+
+	LEFT JOIN Attendance A ON A.studentId = S.studentId
+
 	LEFT JOIN ProfileFieldValues SCH
 		ON SCH.userId = S.studentId
 		AND SCH.fieldName = 'PROGRAM_HOURS_SCHEDULED'
-    
+
 	WHERE S.isActive IN (1, 12)
-    AND SCH.fieldValue - ATT.fieldValue > 0
     AND S.<ADMINID>
+	AND A.isActive = 1
+	AND A.attendanceDate >= R.startDate
+	AND S.studentCampus = (SELECT campusCode FROM Teachers WHERE teacherId = [?USERID])
+
+	GROUP BY R.registrationId
 ) t1
 
 
