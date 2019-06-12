fb976a11d2503974dc2ab4328f6b56f0edb27aeb
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-06-12 16:18:35 -0400

    Update INS Your_Class_Attendance.sql
    matches shell code

--- Start Page Widgets BENES/INS Your_Class_Attendance.sql
+++ Start Page Widgets BENES/INS Your_Class_Attendance.sql
@@ -1,4 +1,4 @@
--- [BENES] INS Your Class Attendance
+-- [SHELL] INS Your Class Attendance
 -- Kelly MJ 06/12/2019 - whole lot of changes, refer to github.com/tinytacooo/Prestige-SQL-Projects for full history
 
 SELECT C.className AS 'Class'
@@ -11,34 +11,34 @@ SELECT C.className AS 'Class'
               THEN 'Clocked In'
          ELSE 'Clocked Out'
        END AS 'Status'
-    , COALESCE(DATE_FORMAT(CP.lastPunch, '%m/%d @ %h:%i'), ' -- ') AS 'Last Punch Today'
+    , COALESCE(DATE_FORMAT(CP.lastPunch, '%h:%i %p'), ' -- ') AS 'Last Punch Today'
+    -- , COALESCE(CP.punches, '--') AS 'Punches Today'
 
-FROM (SELECT R.studentId
-           , R.registrationId
-           , MAX(R.startDate)
-      FROM Registrations R
-      WHERE R.regStatus = 1
-        AND R.isActive = 1
-        AND R.enrollmentSemesterId = 4000441
-        AND R.<ADMINID>
-      GROUP BY R.studentId
-      ) REG
-
-INNER JOIN Students S ON REG.studentId = S.studentId
+FROM Students S
+INNER JOIN ( SELECT studentId, MAX(registrationId) AS maxReg FROM Registrations WHERE isActive = 1 GROUP BY studentId ) RR
+    ON RR.studentId = S.studentId
+INNER JOIN Registrations R ON R.studentId = S.studentId
+    AND R.registrationId = RR.maxReg
 INNER JOIN ClassStudentReltn CSR ON S.studentId = CSR.studentId
 INNER JOIN Classes C ON CSR.classId = C.classId
-LEFT JOIN (SELECT userId, MAX(punchTime) AS lastPunch, COUNT(clockPunchId) AS punchCount
+LEFT JOIN (SELECT userId
+                , GROUP_CONCAT(DATE_FORMAT(punchTime, '%h:%i %p') SEPARATOR '; ') AS punches
+                , MAX(punchTime) AS lastPunch
+                , COUNT(clockPunchId) AS punchCount
 			FROM ClockPunches
             WHERE isActive IN (1, 2)
             AND DATE(punchTime) = CURDATE()
             GROUP BY userId) CP
 	ON CP.userId = S.studentId
 
-WHERE C.classId IN (SELECT classId FROM ClassTeacherReltn WHERE isActive = 1 AND teacherId = [USERID])
+WHERE R.isActive = 1
+AND R.startDate <= CURDATE()
+AND C.classId IN (SELECT classId FROM ClassTeacherReltn WHERE isActive = 1 AND teacherId = [USERID])
 AND S.isActive IN (1, 12)
 AND CSR.isActive = 1
 AND CSR.status < 2
 AND C.isActive = 1
+AND C.<ADMINID>
 
 GROUP BY S.studentId
 
