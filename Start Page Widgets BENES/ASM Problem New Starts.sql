a73b9e181fa3b353be7e61b24ac0fddecd1301a7
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-06-17 12:47:58 -0400

    Create ASM Problem New Starts.sql

---
+++ Start Page Widgets BENES/ASM Problem New Starts.sql
@@ -0,0 +1,6 @@
+-- [BENES] ASM Problem New Starts
+-- Kelly MJ  |  06/17/2019
+-- Displays new starts (start date <= 30 days before current date) who have not attended their first days of class OR have < 90% attendance.
+-- Students are matched to admissions officer's campus.
+
+SELECT 
