29dd0b4dd878f267d93a412d2a0c4d13627acdf6
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-06-24 12:25:39 -0400

    Update Lead List.sql
    Fixed Campus selection. Only use campus name now, not zip code.

--- Query Reports UNIVERSAL/Lead List.sql
+++ Query Reports UNIVERSAL/Lead List.sql
@@ -2,9 +2,7 @@
 -- Kelly MJ  |  6/17/2019
 -- Displays a list of leads. User input: lead type, lead campus
 
-SELECT IF('[?Campus]' <> ''
-            , (SELECT DISTINCT(campusName) FROM Campuses WHERE (INSTR(REPLACE(LOWER(campusName), ' ', ''), REPLACE(LOWER('[?Campus]'), ' ', '')) OR '[?Campus]' = campusCode))
-            , CMP.fieldValue) AS Campus
+SELECT CMP.fieldValue AS Campus
     , CT.typeName
     , CONCAT('<a target="_blank" href="admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '">', C.lastName, ', ', C.firstName, '</a>') AS Name
     , IF(RD.fieldValue < CURDATE(), DATE_FORMAT(RD.fieldValue, '<div style="background-color: #f09595;">%m/%d/%Y</div>'), DATE_FORMAT(RD.fieldValue, '%m/%d/%Y')) AS 'Recontact Date'
@@ -33,8 +31,7 @@ AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9'
 AND C.<ADMINID>
 -- user inputs
 AND IF('[?Campus]' <> ''
-		, ( INSTR(REPLACE(LOWER(CMP.fieldValue), ' ', ''), REPLACE(LOWER('[?Campus]'), ' ', ''))
-            OR EXISTS (SELECT * FROM Campuses WHERE '[?Campus]' = campusCode AND campusName = CMP.fieldValue ) )
+		, ( INSTR(REPLACE(LOWER(CMP.fieldValue), ' ', ''), REPLACE(LOWER('[?Campus]'), ' ', '')) )
 		, C.<ADMINID> /* dummy condition */ )
 AND IF('[?Lead Type{Show All|Show All|1.|1. New Lead|2.|2. Attempted Contact|3.|3. Contacted/Working|4.|4. 1st Appointment/School Tour|5.|5. 2nd Appointment/Toured|6.|6. 3rd Appointment/Financial Aid|7.|7. Pending Enrollment|8.|8. Enrolled|9.|9. Nurturing|86|86. Lost/Not Interested}]' = 'Show All'
     , C.<ADMINID> /* dummy condition */
