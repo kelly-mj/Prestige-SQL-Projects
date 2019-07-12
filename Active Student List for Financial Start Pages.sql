cfb3aadd1ff5762c2411fdc23ea6775605e1da15
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-07-12 14:57:35 -0400

    Update Active Student List for Financial Start Pages.sql
     - Changed student name format to "lastname, firstname"
     - Uses PFV PROGAM_HOURS_ATTENDED instead of sum of records from Attendance table
     - Added schedule type
     - Ordered by last name

--- Active Student List for Financial Start Pages.sql
+++ Active Student List for Financial Start Pages.sql
@@ -1,32 +1,27 @@
+-- [BENES] Active Student List for Financial Start Pages
+-- Unknown author  |  Last updated 7/12/2019
+
 SELECT
-	CONCAT('<a href="admin_view_student.jsp?studentid=', CAST(SDT.studentId AS CHAR), '">', CAST(SDT.firstName AS CHAR), ' ', CAST(SDT.lastName AS CHAR), '</a>') AS Name,
-	-- REG.registrationId AS KeyZ,
-	-- ATD.subjectId AS Subject_ID,
-	-- ATD.classId AS Class_ID,
+	CONCAT('<a href="admin_view_student.jsp?studentid=', CAST(SDT.studentId AS CHAR), '">', CAST(SDT.lastName AS CHAR), ', ', CAST(SDT.firstName AS CHAR), '</a>') AS Name,
 	PGM.programmeName AS Program_Name,
 	CMP.campusName AS Campus_Name,
+	PFV.fieldValue AS 'Schedule Type',
     SSS.StatusName,
-	FORMAT(SUM(ATD.duration),2) AS Hours_Attended
+	(SELECT MAX(fieldValue) FROM ProfileFieldValues WHERE userId = SDT.studentId AND fieldName = 'PROGRAM_HOURS_ATTENDED') AS Hours_Attended
 
-FROM Attendance ATD
-	INNER JOIN Registrations	REG ON ATD.studentId = REG.studentId AND  REG.isActive = 1
+
+FROM Students SDT
+	INNER JOIN Registrations	REG ON SDT.studentId = REG.studentId AND  REG.isActive = 1
 	INNER JOIN Programmes 	PGM ON REG.programmeId = PGM.programmeId AND  PGM.isActive = 1
-	INNER JOIN Students 		SDT ON REG.studentId = SDT.studentId
 	INNER JOIN Campuses          CMP ON SDT.studentCampus = CMP.campusCode AND CMP.isActive = 1
-        INNER JOIN StatusSequences SSS ON SSS.statusId = SDT.isActive AND SSS.seqNum = 1 AND  SSS.isActive = 1 AND SSS.<ADMINID>
+    INNER JOIN StatusSequences SSS ON SSS.statusId = SDT.isActive AND SSS.seqNum = 1 AND  SSS.isActive = 1 AND SSS.<ADMINID>
+	LEFT JOIN ProfileFieldValues PFV ON PFV.userId = SDT.studentId AND PFV.isActive=1 AND PFV.fieldName = 'ATTENDANCE_INFO'
 
 WHERE
 	REG.<ADMINID>						AND
 	REG.isActive = 1				AND
 	REG.enrollmentSemesterId = 4000441	AND
-	SSS.StatusID AND SSS.statusID NOT IN (0,3) AND
-	ATD.subjectId IN (SELECT GSR.subjectId
-			FROM CourseGroups CGP
-			INNER JOIN GroupSubjectReltn GSR ON CGP.courseGroupId=GSR.courseGroupId AND GSR.isActive=1
-			WHERE REG.programmeId = CGP.programmeId and CGP.isActive=1)
-										AND
-	ATD.classId IN (SELECT DISTINCT CRS.classId
-			From ClassStudentReltn CRS
-			Where CRS.studentId = ATD.studentId AND CRS.isActive=1)
-GROUP BY ATD.studentId
-ORDER BY CMP.campusName, SDT.firstName
+	SSS.StatusID AND SSS.statusID NOT IN (0,3)
+
+GROUP BY SDT.studentId
+ORDER BY CMP.campusName, SDT.lastName
