5f7cb80f494fa41e18a4f5179bcabd10b3e8cae7
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-07-12 14:15:06 -0400

    Update AMS Prospects for Your Campus.sql
    Changed from old Bene's code

--- Start Page Widgets CORTIVA/AMS Prospects for Your Campus.sql
+++ Start Page Widgets CORTIVA/AMS Prospects for Your Campus.sql
@@ -1,20 +1,22 @@
-SELECT
-CONCAT('<a href="admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '">', CAST(C.firstName AS CHAR), ' ', CAST(C.lastName AS CHAR), '</a>') AS Name,
+-- [Cortiva] AMS, CDIR Prospects For Your Campus
+-- Kelly MJ  |  7/7/2019
+-- Shows a list of all leads (expect 86. Lost, 8. Enrolled, 9. Started) 
 
-	CT.typeName AS Status,
-	IFNULL(PV.fieldValue,"") AS CAMPUS,
-	DATE_FORMAT(C.lastUpdateDtTm, '%m-%d %h:%i') AS 'Last Update'
+SELECT IFNULL(CMP.campusName, '<div style="">No campus</div>') AS Campus
+	, CT.typeName AS Status
+	, CONCAT('<a href="admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '">', CAST(C.lastName AS CHAR), ', ', CAST(C.firstName AS CHAR), '</a>') AS Name
+	, DATE_FORMAT(C.lastUpdateDtTm, '%m-%d %h:%i') AS 'Last Update'
         
-FROM
-	Contacts C
-INNER JOIN ContactTypes CT ON C.contactTypeId = CT.contactTypeId AND CT.isActive=1
-LEFT JOIN ProfileFieldValues PV ON PV.userId=C.contactId AND PV.userType=99 AND PV.fieldName="CAMPUS"  AND PV.isActive=1 AND PV.<ADMINID>
-WHERE
-	C.<ADMINID> AND
-	C.contactTypeId <> 4000039 AND -- not vendor
-        C.contactTypeId <> 4000052 AND -- not Salon Customer
-	C.contactTypeId <> 4000042 AND -- not nurturing
- 	C.contactTypeId <> 4000047 AND -- not Gains
-        C.contactTypeId <> 4000049 AND -- not Future Attending Date
-        C.contactTypeId <> 4000050 AND -- not lost       
-	C.contactTypeId <> 4000046  -- not enrolled student
+FROM Contacts C
+INNER JOIN ContactTypes CT ON C.contactTypeId = CT.contactTypeId
+LEFT JOIN Campuses CMP ON CMP.campusCode = C.campusCode
+
+WHERE C.isActive = 1
+AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '0')
+AND SUBSTRING(CT.typeName, 1, 2) <> '86'
+AND CT.isActive=1
+AND (C.campusCode = -1
+	 OR C.campusCode IN (SELECT campusCode FROM SubAdmins WHERE subAdminId = [USERID]))
+AND C.<ADMINID>
+
+ORDER BY CMP.campusName, CT.typeName, C.lastName
