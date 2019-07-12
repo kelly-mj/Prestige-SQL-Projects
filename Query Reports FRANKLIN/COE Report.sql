91a7ae8b3a4fb122959df138f6a21003e491585a
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-07-12 16:20:31 -0400

    Update COE Report.sql
     - Changed hour rounding
     - Changed "Hours to Date" column to not actually calculate hours to date, because apparently, that's not what that means

--- Query Reports FRANKLIN/COE Report.sql
+++ Query Reports FRANKLIN/COE Report.sql
@@ -11,7 +11,7 @@
     , COALESCE(ROUND(SUM(CASE WHEN A.attendanceDate >= CONCAT('[?Year (YYYY)]-', '[?Month (MM)]', '-01')
 					 AND A.attendanceDate <= LAST_DAY(CONCAT('[?Year (YYYY)]-', '[?Month (MM)]', '-01'))
 						THEN A.duration END), 1), 0) 'Hours Passed This Month'
-    , ROUND(SUM(A.duration), 1) 'Hours Passed to Date'
+    , COALESCE(ROUND(SUM(CASE WHEN A.attendanceDate <=  LAST_DAY(CONCAT('[?Year (YYYY)]-', '[?Month (MM)]', '-01')) THEN A.duration END), 1), 0) 'Hours Passed to Date'
 
 FROM Students S
 INNER JOIN (SELECT studentId, MAX(registrationId) AS maxReg FROM Registrations WHERE isActive = 1 GROUP BY studentId) RR
@@ -19,16 +19,19 @@ INNER JOIN (SELECT studentId, MAX(registrationId) AS maxReg FROM Registrations W
 INNER JOIN Registrations R ON R.studentId = S.studentId
 	AND R.registrationId = RR.maxReg
 INNER JOIN Programmes P ON P.programmeId = R.programmeId
-LEFT JOIN (SELECT studentId, attendanceDate, duration
-	FROM Attendance
+LEFT JOIN (SELECT studentId, attendanceDate, duration, classId
+	FROM Attendance A
     WHERE isActive = 1
     AND attendanceDate <= CURDATE()) A
     ON A.studentId = S.studentId
+LEFT JOIN ClassStudentReltn CSR ON CSR.classId = A.classId
+	AND CSR.studentId = A.studentId
 
 WHERE (R.graduationDate IS NULL OR
 	   R.graduationDate BETWEEN CONCAT('[?Year (YYYY)]-', LPAD('[?Month (MM)]', 2, '0'), '-01') AND LAST_DAY(CONCAT('[?Year (YYYY)]-', LPAD('[?Month (MM)]', 2, '0'), '-01')) )
 AND P.programmeName = '[?Program{Aesthetics|Aesthetics|Cosmetology|Cosmetology}]'
 AND A.attendanceDate >= R.startDate
+AND CSR.isActive = 1
 AND S.<ADMINID>
 
 GROUP BY S.studentId
@@ -46,11 +49,16 @@ UNION
 	, COALESCE(ROUND(SUM(hoursLastMonth)+SUM(hoursThisMonth), 1), 0)
 
 FROM (
-	SELECT S.studentId
-		, COALESCE(SUM(CASE WHEN A.attendanceDate <  CONCAT('[?Year (YYYY)]-', '[?Month (MM)]', '-01') THEN A.duration END), 0) hoursLastMonth
-		, COALESCE(SUM(CASE WHEN A.attendanceDate >= CONCAT('[?Year (YYYY)]-', '[?Month (MM)]', '-01')
+	SELECT CONCAT('<a target="_blank" href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', CAST(S.studentId AS CHAR), '</a>') AS Name
+		, S.lastName
+		, S.firstName
+	    , SUBSTRING(S.ssn, 8, 12) 'Last 4 of SSN#'
+	    , DATE_FORMAT(R.startDate, '%m/%d/%Y') 'Enrollment Date'
+	    , SUBSTRING(P.programmeName, 1, 1) 'Course of Study'
+	    , COALESCE(ROUND(SUM(CASE WHEN A.attendanceDate <  CONCAT('[?Year (YYYY)]-', '[?Month (MM)]', '-01') THEN A.duration END), 1), 0) hoursLastMonth
+	    , COALESCE(ROUND(SUM(CASE WHEN A.attendanceDate >= CONCAT('[?Year (YYYY)]-', '[?Month (MM)]', '-01')
 						 AND A.attendanceDate <= LAST_DAY(CONCAT('[?Year (YYYY)]-', '[?Month (MM)]', '-01'))
-							THEN A.duration END), 0) hoursThisMonth
+							THEN A.duration END), 1), 0) hoursThisMonth
 
 	FROM Students S
 	INNER JOIN (SELECT studentId, MAX(registrationId) AS maxReg FROM Registrations WHERE isActive = 1 GROUP BY studentId) RR
@@ -58,18 +66,20 @@ FROM (
 	INNER JOIN Registrations R ON R.studentId = S.studentId
 		AND R.registrationId = RR.maxReg
 	INNER JOIN Programmes P ON P.programmeId = R.programmeId
-	LEFT JOIN (SELECT studentId, attendanceDate, duration
+	LEFT JOIN (SELECT studentId, attendanceDate, duration, classId
 		FROM Attendance
-		WHERE isActive = 1) AS A
-		ON A.studentId = S.studentId
+	    WHERE isActive = 1
+	    AND attendanceDate <= CURDATE()) A
+	    ON A.studentId = S.studentId
+	LEFT JOIN ClassStudentReltn CSR ON CSR.classId = A.classId
+		AND CSR.studentId = A.studentId
 
-	WHERE S.isActive IN (1, 12)
+	WHERE (R.graduationDate IS NULL OR
+		   R.graduationDate BETWEEN CONCAT('[?Year (YYYY)]-', LPAD('[?Month (MM)]', 2, '0'), '-01') AND LAST_DAY(CONCAT('[?Year (YYYY)]-', LPAD('[?Month (MM)]', 2, '0'), '-01')) )
 	AND P.programmeName = '[?Program{Aesthetics|Aesthetics|Cosmetology|Cosmetology}]'
 	AND A.attendanceDate >= R.startDate
+	AND CSR.isActive = 1
 	AND S.<ADMINID>
 
-    GROUP BY S.studentId) t1
-
-    LEFT JOIN ProfileFieldValues PFV
-		ON PFV.userId = t1.studentId
-        AND PFV.fieldName = 'PROGRAM_HOURS_ATTENDED')
+	GROUP BY S.studentId) t1
+)
