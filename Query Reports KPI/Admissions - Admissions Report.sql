940366bf3bbd03ae25080b5edc8903679b269ddb
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-07-29 15:02:43 -0400

    Update Admissions - Admissions Report.sql
    corrected

--- Query Reports KPI/Admissions - Admissions Report.sql
+++ Query Reports KPI/Admissions - Admissions Report.sql
@@ -2,10 +2,10 @@
 -- Kelly MJ  |  7/29/2019
 
 -- adaptions for testing on Benestest:
-   -- '5. Appointment Set' --> '3. Mailed Catalog'
-   -- '6. Interviewed'     --> '6. Nurturing'
-   -- '7. Applied'         --> '5. Working'
-   -- '8. Enrolled'        --> '2. Left Message'
+   -- '5. Appointment Set' --> '5.  Working'
+   -- '6. Interviewed'     --> '6.  Nurturing'
+   -- '7. Applied'         --> '2.  Left Message'
+   -- '8. Enrolled'        --> '3.  Mailed Catalog'
 
 SELECT IF('[?Aggregate?{No|No|Yes|Yes}]' = 'No', CMP.campusName, 'All Campuses') AS Campus
     , t1.type AS 'Type'
@@ -19,7 +19,7 @@ INNER JOIN (
         , 'Lead to Appointment' AS type
         , SUM(IF(DATE(C.creationDtTm) >= '[?From Date]' AND DATE(C.creationDtTm) <= '[?To Date]'
                     , 1, 0)) AS l
-        , SUM(IF((CT.typeName = '3. Mailed Catalog' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , SUM(IF((CT.typeName = '5. Appointment Set' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR USR.toUserId IS NOT NULL
                     , 1, 0)) AS r
         , 'L to A' AS label
@@ -28,7 +28,7 @@ INNER JOIN (
     LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
     LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '3. Mailed Catalog'
+                WHERE T.typeName = '5. Appointment Set'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') USR
         ON USR.toUserId = C.contactId
 
@@ -38,10 +38,10 @@ INNER JOIN (
     UNION /* Appointment to Interview */
     (SELECT IF('[?Aggregate?{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
         , 'Appointment to Interview' AS type
-        , SUM(IF((CT.typeName = '3. Mailed Catalog' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , SUM(IF((CT.typeName = '5. Appointment Set' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR L.toUserId IS NOT NULL
                     , 1, 0)) AS l
-        , SUM(IF((CT.typeName = '6. Nurturing' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , SUM(IF((CT.typeName = '6. Interviewed' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR R.toUserId IS NOT NULL
                     , 1, 0)) AS r
         , 'A to I' AS label
@@ -50,12 +50,12 @@ INNER JOIN (
     LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
     LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '3. Mailed Catalog'
+                WHERE T.typeName = '5. Appointment Set'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') L
         ON L.toUserId = C.contactId
     LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '6. Nurturing'
+                WHERE T.typeName = '6. Interviewed'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') R
         ON R.toUserId = C.contactId
 
@@ -64,10 +64,10 @@ INNER JOIN (
 
     UNION /* Interview to Application */
     (SELECT IF('[?Aggregate?{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
-        , 'Appointment to Interview' AS type
-        , SUM(IF((CT.typeName = '6. Nurturing' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , 'Interview to Application' AS type
+        , SUM(IF((CT.typeName = '6. Interviewed' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR L.toUserId IS NOT NULL, 1, 0)) AS l
-        , SUM(IF((CT.typeName = '5. Working' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , SUM(IF((CT.typeName = '7. Applied' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR R.toUserId IS NOT NULL, 1, 0)) AS r
         , 'I to APP' AS label
         , 3 AS Ord
@@ -75,64 +75,12 @@ INNER JOIN (
     LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
     LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '6. Nurturing'
+                WHERE T.typeName = '6. Interviewed'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') L
         ON L.toUserId = C.contactId
     LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '5. Working'
-                AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') R
-        ON R.toUserId = C.contactId
-
-    WHERE C.isActive = 1
-    GROUP BY C.campusCode)
-
-    UNION /* Lead to Application */
-    (SELECT IF('[?Aggregate?{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
-        , 'Lead to Application' AS type
-        , SUM(IF(DATE(C.creationDtTm) >= '[?From Date]' AND DATE(C.creationDtTm) <= '[?To Date]'
-                    , 1
-                    , 0)) AS l
-        , SUM(IF((CT.typeName = '5. Working' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
-                OR USR.toUserId IS NOT NULL
-                    , 1
-                    , 0)) AS r
-        , 'L to APP' AS label
-        , 4 AS Ord
-    FROM Contacts C
-    LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
-    LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
-                INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '5. Working'
-                AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') USR
-        ON USR.toUserId = C.contactId
-
-    WHERE C.isActive = 1
-    GROUP BY C.campusCode)
-
-    UNION /* Application to Enrollment */
-    (SELECT IF('[?Aggregate?{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
-        , 'Appointment to Interview' AS type
-        , SUM(IF((CT.typeName = '5. Working' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
-                OR L.toUserId IS NOT NULL
-                    , 1
-                    , 0)) AS l
-        , SUM(IF((CT.typeName = '2. Left Message' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
-                OR R.toUserId IS NOT NULL
-                    , 1
-                    , 0)) AS r
-        , 'APP to E' AS label
-        , 5 AS Ord
-    FROM Contacts C
-    LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
-    LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
-                INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '5. Working'
-                AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') L
-        ON L.toUserId = C.contactId
-    LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
-                INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '2. Left Message'
+                WHERE T.typeName = '7. Applied'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') R
         ON R.toUserId = C.contactId
 
@@ -144,7 +92,55 @@ INNER JOIN (
         , 'Lead to Application' AS type
         , SUM(IF(DATE(C.creationDtTm) >= '[?From Date]' AND DATE(C.creationDtTm) <= '[?To Date]'
                     , 1, 0)) AS l
-        , SUM(IF((CT.typeName = '2. Left Message' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , SUM(IF((CT.typeName = '7. Applied' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+                OR USR.toUserId IS NOT NULL
+                    , 1, 0)) AS r
+        , 'L to APP' AS label
+        , 4 AS Ord
+    FROM Contacts C
+    LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
+    LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
+                INNER JOIN ContactTypes T ON T.contactTypeId = U.status
+                WHERE T.typeName = '7. Applied'
+                AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') USR
+        ON USR.toUserId = C.contactId
+
+    WHERE C.isActive = 1
+    GROUP BY C.campusCode)
+
+    UNION /* Application to Enrollment */
+    (SELECT IF('[?Aggregate?{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
+        , 'Application to Enrollment' AS type
+        , SUM(IF((CT.typeName = '7. Applied' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+                OR L.toUserId IS NOT NULL
+                    , 1, 0)) AS l
+        , SUM(IF((CT.typeName = '8. Enrolled' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+                OR R.toUserId IS NOT NULL
+                    , 1, 0)) AS r
+        , 'APP to E' AS label
+        , 5 AS Ord
+    FROM Contacts C
+    LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
+    LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
+                INNER JOIN ContactTypes T ON T.contactTypeId = U.status
+                WHERE T.typeName = '7. Applied'
+                AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') L
+        ON L.toUserId = C.contactId
+    LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
+                INNER JOIN ContactTypes T ON T.contactTypeId = U.status
+                WHERE T.typeName = '8. Enrolled'
+                AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') R
+        ON R.toUserId = C.contactId
+
+    WHERE C.isActive = 1
+    GROUP BY C.campusCode)
+
+    UNION /* Lead to Enrollment */
+    (SELECT IF('[?Aggregate?{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
+        , 'Lead to Enrollment' AS type
+        , SUM(IF(DATE(C.creationDtTm) >= '[?From Date]' AND DATE(C.creationDtTm) <= '[?To Date]'
+                    , 1, 0)) AS l
+        , SUM(IF((CT.typeName = '8. Enrolled' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR USR.toUserId IS NOT NULL
                     , 1, 0)) AS r
         , 'L to E' AS label
@@ -153,7 +149,7 @@ INNER JOIN (
     LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
     LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '2. Left Message'
+                WHERE T.typeName = '8. Enrolled'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') USR
         ON USR.toUserId = C.contactId
 
