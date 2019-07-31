bdf06415179158a11b4085d46f26494fe8f82f96
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-07-31 00:28:27 -0400

    Added "Aggregate?" option

--- Query Reports KPI/Admissions - Show Rate.sql
+++ Query Reports KPI/Admissions - Show Rate.sql
@@ -1,15 +1,15 @@
 -- [HWD] KPI - Admissions - Cancel-Show Rates
 -- Kelly MJ  |  7/22/2019
 
-SELECT CMP.campusName
-    , CONCAT(COALESCE(APP.num, 0), ' : ', COALESCE(C.num, 0)) 'Applications : Cancels'
-    , CONCAT(ROUND(COALESCE(100*COALESCE(C.num, 0)/COALESCE(APP.num, 0), 0), 1), '%') 'Cancel Rate'
-    , CONCAT(COALESCE(APP.num, 0), ' : ', COALESCE(S.num, 0)) 'Applications : Starts'
-    , CONCAT(ROUND(COALESCE(100*COALESCE(S.num, 0)/COALESCE(APP.num, 0), 0), 1), '%') 'Show Rate'
+SELECT IF('[?Aggregate{No|No|Yes|Yes}]' = 'No', CMP.campusName, 'All Campuses') AS 'Campus'
+    , CONCAT(COALESCE(SUM(APP.num), 0), ' : ', COALESCE(SUM(C.num), 0)) 'Applications : Cancels'
+    , CONCAT(ROUND(COALESCE(100*COALESCE(SUM(C.num), 0)/COALESCE(SUM(APP.num), 0), 0), 1), '%') 'Cancel Rate'
+    , CONCAT(COALESCE(SUM(APP.num), 0), ' : ', COALESCE(SUM(S.num), 0)) 'Applications : Starts'
+    , CONCAT(ROUND(COALESCE(100*COALESCE(SUM(S.num), 0)/COALESCE(SUM(APP.num), 0), 0), 1), '%') 'Show Rate'
 
 FROM Campuses CMP
 LEFT JOIN (
-    SELECT C.campusCode
+    SELECT IF('[?Aggregate{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
         , COALESCE(COUNT(DISTINCT C.contactId), 0) AS num
         FROM Contacts C
         LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
@@ -22,32 +22,27 @@ LEFT JOIN (
         AND C.<ADMINID>
         AND ((CT.typeName = '7. Applied' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR USR.toUserId IS NOT NULL)
-        AND IF('[?Campus]' <> ''
-                , ( EXISTS (SELECT * FROM Campuses WHERE INSTR(REPLACE(LOWER(campusName), ' ', ''), REPLACE(LOWER('[?Campus]'), ' ', '')) AND campusCode = C.campusCode) OR C.campusCode = '[?Campus]')
-                , C.<ADMINID> /* dummy condition */ )
         GROUP BY C.campusCode ) APP
     ON APP.campusCode = CMP.campusCode
 LEFT JOIN (
-    SELECT C.campusCode
+    SELECT IF('[?Aggregate{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
         , COALESCE(COUNT(DISTINCT C.contactId), 0) AS num
         FROM Contacts C
         LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
-        LEFT JOIN (SELECT U.toUserId FROM UserStatusRecords U
+        LEFT JOIN (SELECT U.toUserId, MAX(U.updateDtTm) FROM UserStatusRecords U
                     INNER JOIN ContactTypes T ON T.contactTypeId = U.status
                     WHERE T.typeName = '86. Lost - Not Interested - Cancel'
-                    AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') USR
+                    AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]'
+                    GROUP BY U.toUserId) USR
             ON USR.toUserId = C.contactId
         WHERE C.isActive = 1
         AND C.<ADMINID>
         AND ((CT.typeName = '86. Lost - Not Interested - Cancel' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR USR.toUserId IS NOT NULL)
-        AND IF('[?Campus]' <> ''
-                , ( EXISTS (SELECT * FROM Campuses WHERE INSTR(REPLACE(LOWER(campusName), ' ', ''), REPLACE(LOWER('[?Campus]'), ' ', '')) AND campusCode = C.campusCode) OR C.campusCode = '[?Campus]')
-                , C.<ADMINID> /* dummy condition */ )
         GROUP BY C.campusCode ) C
     ON C.campusCode = CMP.campusCode
 LEFT JOIN (
-    SELECT C.campusCode
+    SELECT IF('[?Aggregate{No|No|Yes|Yes}]' = 'No', C.campusCode, (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1)) AS campusCode
         , COALESCE(COUNT(DISTINCT C.contactId), 0) AS num
         FROM Contacts C
         LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
@@ -60,13 +55,12 @@ LEFT JOIN (
         AND C.<ADMINID>
         AND ((CT.typeName = '9. Started Student' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR USR.toUserId IS NOT NULL)
-        AND IF('[?Campus]' <> ''
-                , ( EXISTS (SELECT * FROM Campuses WHERE INSTR(REPLACE(LOWER(campusName), ' ', ''), REPLACE(LOWER('[?Campus]'), ' ', '')) AND campusCode = C.campusCode) OR C.campusCode = '[?Campus]')
-                , C.<ADMINID> /* dummy condition */ )
         GROUP BY C.campusCode ) S
     ON S.campusCode = CMP.campusCode
 
-WHERE IF('[?Campus]' <> ''
-			, INSTR(REPLACE(LOWER(CMP.campusName), ' ', ''), REPLACE(LOWER('[?Campus]'), ' ', '')) OR CMP.campusCode = '[?Campus]'
-			, CMP.isActive = 1 /* dummy condition */ )
+WHERE IF('[?Aggregate{No|No|Yes|Yes}]' = 'No'
+            , CMP.isActive = 1 /* dummy condition */
+            , CMP.campusCode = (SELECT MAX(campusCode) FROM Campuses WHERE isActive = 1))
 AND CMP.isActive = 1
+
+GROUP BY CMP.campusCode
