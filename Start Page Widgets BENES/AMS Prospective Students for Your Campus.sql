01367ee20855079b7b7399a93cbc62d4c8360ec3
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-08-07 12:22:16 -0400

    Update AMS Prospective Students for Your Campus.sql
    replaced old code - new code searches for leads whose types begin with an integer.

--- Start Page Widgets BENES/AMS Prospective Students for Your Campus.sql
+++ Start Page Widgets BENES/AMS Prospective Students for Your Campus.sql
@@ -2,52 +2,21 @@
 -- Edit: Kelly MJ  |  1/2/2019
 -- The two halves of these codes were thrown into two separate front page reports and are linked to by a 'Widget Links' report in Admissions' front page.
 
-/*
- *  Brooksville Report
- */
-SELECT IFNULL(PV.fieldValue,"") AS CAMPUS
-	, CONCAT('<a href="admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '">', CAST(C.firstName AS CHAR), ' ', CAST(C.lastName AS CHAR), '</a>') AS Name
+SELECT IFNULL(CMP.campusName, '<div style="">No campus</div>') AS Campus
 	, CT.typeName AS Status
-	, DATE_FORMAT(C.lastUpdateDtTm, '%m/%d/%Y' ) AS 'Last Update'
-        
+	, CONCAT('<a href="admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '">', CAST(C.lastName AS CHAR), ', ', CAST(C.firstName AS CHAR), '</a>') AS Name
+	, DATE_FORMAT(C.creationDtTm, '%m-%d-%y') AS 'Date Added'
+	, DATE_FORMAT(C.lastUpdateDtTm, '%m-%d %h:%i') AS 'Last Update'
+
 FROM Contacts C
-INNER JOIN ContactTypes CT ON C.contactTypeId = CT.contactTypeId AND CT.isActive=1
-LEFT JOIN ProfileFieldValues PV ON PV.userId=C.contactId AND PV.userType=99 AND PV.fieldName="CAMPUS"  AND PV.isActive=1 AND PV.<ADMINID>
-WHERE
-	C.<ADMINID> AND
-	C.contactTypeId <> 4000039 AND -- not vendor
-        C.contactTypeId <> 4000052 AND -- not Salon Customer
-	C.contactTypeId <> 4000042 AND -- not nurturing
- 	C.contactTypeId <> 4000047 AND -- not Gains
-        C.contactTypeId <> 4000049 AND -- not Future Attending Date
-        C.contactTypeId <> 4000050 AND -- not lost       
-	C.contactTypeId <> 4000046  -- not enrolled student
-	AND PV.fieldValue = 'Brooksville'
-   
-ORDER BY CAMPUS, status, C.lastUpdateDtTm DESC
+INNER JOIN ContactTypes CT ON C.contactTypeId = CT.contactTypeId
+LEFT JOIN Campuses CMP ON CMP.campusCode = C.campusCode
 
+WHERE C.isActive = 1
+AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '0')
+AND SUBSTRING(CT.typeName, 1, 2) <> '86'
+AND CT.isActive = 1
+#AND C.<ADMINID>
+AND C.campusCode = (SELECT campusCode FROM SubAdmins WHERE subAdminId = [USERID])
 
-/*
- *  Spring Hill Report
- */
-
-SELECT IFNULL(PV.fieldValue,"") AS CAMPUS
-	, CONCAT('<a href="admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '">', CAST(C.firstName AS CHAR), ' ', CAST(C.lastName AS CHAR), '</a>') AS Name
-	, CT.typeName AS Status
-	, DATE_FORMAT(C.lastUpdateDtTm, '%m/%d/%Y' ) AS 'Last Update'
-        
-FROM Contacts C
-INNER JOIN ContactTypes CT ON C.contactTypeId = CT.contactTypeId AND CT.isActive=1
-LEFT JOIN ProfileFieldValues PV ON PV.userId=C.contactId AND PV.userType=99 AND PV.fieldName="CAMPUS"  AND PV.isActive=1 AND PV.<ADMINID>
-WHERE
-	C.<ADMINID> AND
-	C.contactTypeId <> 4000039 AND -- not vendor
-        C.contactTypeId <> 4000052 AND -- not Salon Customer
-	C.contactTypeId <> 4000042 AND -- not nurturing
- 	C.contactTypeId <> 4000047 AND -- not Gains
-        C.contactTypeId <> 4000049 AND -- not Future Attending Date
-        C.contactTypeId <> 4000050 AND -- not lost       
-	C.contactTypeId <> 4000046  -- not enrolled student
-	AND PV.fieldValue = 'Spring Hill'
-   
-ORDER BY CAMPUS, status, C.lastUpdateDtTm DESC
+ORDER BY C.creationDtTm DESC
