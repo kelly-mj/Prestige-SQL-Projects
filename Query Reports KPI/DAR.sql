b3f58aa65e31e364143119f9fe9e1e4141c9b2e0
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-08-07 09:45:07 -0400

    Update DAR.sql
     - added appointments count
     - changed hard-coded test date to user input [?Run Date]?
     - removed Ft Lauderdale results
     - changed line 75 to DATE(U.updateDtTm)

--- Query Reports KPI/DAR.sql
+++ Query Reports KPI/DAR.sql
@@ -1,25 +1,46 @@
 -- Flash Report: Attempt 4
 
 SELECT CMP.campusName
-	, DATES.auto_date
-    , t2.I_count
-    , t2.E_count
+	, CONCAT('<span style="width: 15px;">', SUBSTRING(DATE_FORMAT(DATES.auto_date, '%W'), 1, 3), DATE_FORMAT(DATES.auto_date, '</span> %m/%d/%Y')) AS 'Date'
+	, IF(DATES.auto_date IS NULL, NULL, COALESCE(t1.C_count, IF(DATES.auto_date > CURDATE(), NULL, 0))) AS 'Leads'
+    , IF(DATES.auto_date IS NULL, NULL, COALESCE(t2.I_count, IF(DATES.auto_date > CURDATE(), NULL, 0))) AS 'Interviewed'
+    , IF(DATES.auto_date IS NULL, NULL, COALESCE(t2.E_count, IF(DATES.auto_date > CURDATE(), NULL, 0))) AS 'Enrolled'
+    , IF(DATES.auto_date IS NULL, NULL, COALESCE(t3.A_count, IF(DATES.auto_date > CURDATE(), NULL, 0))) AS 'Appointments Tomorrow'
 
 FROM Campuses CMP
+
+/* expand campuses*each_day of the six days in the flash report */
 LEFT JOIN (
-	SELECT DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 2) DAY) AS auto_date, 1 AS active
-	UNION SELECT DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 3) DAY), 1 AS active
-	UNION SELECT DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 4) DAY), 1 AS active
-	UNION SELECT DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 5) DAY), 1 AS active
-	UNION SELECT DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 6) DAY), 1 AS active
-	UNION SELECT DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 7) DAY), 1 AS active
+	SELECT DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 2) DAY) AS auto_date, 1 AS active
+	UNION SELECT DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 3) DAY), 1 AS active
+	UNION SELECT DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 4) DAY), 1 AS active
+	UNION SELECT DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 5) DAY), 1 AS active
+	UNION SELECT DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 6) DAY), 1 AS active
+	UNION SELECT DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 7) DAY), 1 AS active
     UNION SELECT null, 1 AS active
 	) DATES ON DATES.active = CMP.isActive
+
+/* select newly added leads per campus and date */
 LEFT JOIN (
-	SELECT t1.campusCode
-		, t1.updateDtTm
-		, COUNT(DISTINCT t1.I) AS I_count
-		, COUNT(DISTINCT t1.E) AS E_count
+	SELECT C.campusCode
+		, DATE(C.creationDtTm) AS creationDtTm
+		, COUNT(DISTINCT C.contactId) AS C_count
+	FROM Contacts C
+	INNER JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
+	WHERE DATE(C.creationDtTm) >= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 2) DAY)
+	  AND DATE(C.creationDtTm) <= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 7) DAY)
+	  AND C.isActive = 1
+	  AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+	GROUP BY C.campusCode, DATE(C.creationDtTm)
+) t1 ON t1.campusCode = CMP.campusCode
+	AND t1.creationDtTm = DATES.auto_date
+
+/* select leads who were interviewed/enrolled on each campus on each day of the week */
+LEFT JOIN (
+	SELECT t2_a.campusCode
+		, t2_a.updateDtTm
+		, COUNT(DISTINCT t2_a.I) AS I_count
+		, COUNT(DISTINCT t2_a.E) AS E_count
 	FROM (
 		(SELECT C.campusCode
 			, CONCAT(C.firstName, C.lastName) AS 'I'
@@ -31,8 +52,8 @@ LEFT JOIN (
 		INNER JOIN ContactTypes CT ON CT.contactTypeId = U.status
 
 		WHERE CT.typeName = '6. Interviewed'
-		  AND DATE(U.updateDtTm) >= DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 2) DAY)
-		  AND DATE(U.updateDtTm) <= DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 7) DAY)
+		  AND DATE(U.updateDtTm) >= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 2) DAY)
+		  AND DATE(U.updateDtTm) <= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 7) DAY)
 		  #AND U.<ADMINID>
 
 		GROUP BY C.firstName, C.lastName, DATE(U.updateDtTm))
@@ -47,11 +68,11 @@ LEFT JOIN (
 		INNER JOIN ContactTypes CT ON CT.contactTypeId = U.status
 
 		WHERE CT.typeName = '8. Enrolled'
-		  AND DATE(U.updateDtTm) >= DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 2) DAY)
-		  AND DATE(U.updateDtTm) <= DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 7) DAY)
+		  AND DATE(U.updateDtTm) >= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 2) DAY)
+		  AND DATE(U.updateDtTm) <= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 7) DAY)
 		  #AND U.<ADMINID>
 
-		GROUP BY C.firstName, C.lastName, U.updateDtTm)
+		GROUP BY C.firstName, C.lastName, DATE(U.updateDtTm))
 		UNION
 		(SELECT C.campusCode
 			, IF(CT.typeName = '6. Interviewed', CONCAT(C.firstName, C.lastName), NULL) AS 'I'
@@ -62,14 +83,35 @@ LEFT JOIN (
 		INNER JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
 
 		WHERE CT.typeName IN ('6. Interviewed', '8. Enrolled')
-		  AND DATE(C.lastUpdateDtTm) >= DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 2) DAY)
-		  AND DATE(C.lastUpdateDtTm) <= DATE_SUB('2019-07-29', INTERVAL (DAYOFWEEK('2019-07-29') - 7) DAY)
+		  AND DATE(C.lastUpdateDtTm) >= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 2) DAY)
+		  AND DATE(C.lastUpdateDtTm) <= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 7) DAY)
 		GROUP BY C.firstName, C.lastName, DATE(C.lastUpdateDtTm)
 		)
-	) t1
-	GROUP BY t1.campusCode, t1.updateDtTm
+	) t2_a
+	GROUP BY t2_a.campusCode, t2_a.updateDtTm
 ) t2 ON CMP.campusCode = t2.campusCode
 	AND t2.updateDtTm = DATES.auto_date
 
+/* select appointments per campus per day of the week */
+LEFT JOIN  (
+	SELECT SA.campusCode
+		, DATE_SUB(DATE(A.fromDtTm), INTERVAL 1 DAY) AS fromDtTm
+		, COUNT(DISTINCT A.appointmentId) AS A_count
+
+	FROM Appointments A
+	INNER JOIN AppointmentPeopleReltn APR ON APR.appointmentId = A.appointmentId
+	LEFT JOIN SubAdmins SA ON SA.subAdminId = APR.userId
+
+	WHERE APR.userType IN (4, 6)
+	  AND A.isActive = 1
+	  AND APR.isActive = 1
+	  AND DATE(A.fromDtTm) >= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 3) DAY)
+	  AND DATE(A.fromDtTm) <= DATE_SUB('[?Run Date]', INTERVAL (DAYOFWEEK('[?Run Date]') - 8) DAY)
+
+	GROUP BY SA.campusCode, DATE(A.fromDtTm)
+) t3 ON t3.campusCode = CMP.campusCode
+	AND t3.fromDtTm = DATES.auto_date
+
 WHERE CMP.isActive = 1
+  AND CMP.campusName <> 'Ft Lauderdale Beach'
 ORDER BY CMP.campusName, DATES.auto_date
