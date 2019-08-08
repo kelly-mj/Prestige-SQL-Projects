0e8d3cc977d66545fa1ab0a48c2a4b6085529101
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-08-08 10:03:27 -0400

    Update AMS Prospective Students for Your Campus.sql
    removed '8'

--- Start Page Widgets BENES/AMS Prospective Students for Your Campus.sql
+++ Start Page Widgets BENES/AMS Prospective Students for Your Campus.sql
@@ -12,7 +12,7 @@ INNER JOIN ContactTypes CT ON C.contactTypeId = CT.contactTypeId
 LEFT JOIN Campuses CMP ON CMP.campusCode = C.campusCode
 
 WHERE C.isActive = 1
-AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '0')
+AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '0')
 AND SUBSTRING(CT.typeName, 1, 2) <> '86'
 AND CT.isActive = 1
 #AND C.<ADMINID>
