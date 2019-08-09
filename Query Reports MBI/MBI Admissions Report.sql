68eb878ff58cfa1f5788160d48d64c274f352258
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-08-09 14:37:28 -0400

    using conversions sent by Daryl at MBI

--- Query Reports MBI/MBI Admissions Report.sql
+++ Query Reports MBI/MBI Admissions Report.sql
@@ -8,20 +8,20 @@ SELECT t1.type AS 'Type'
             , CONCAT('<span style="display: inline-block; width: 45px; color: red;">', FORMAT(100*COALESCE((SUM(t1.r))/SUM(t1.l), 0), 0), '%</span><span style="color: red;">', t1.label, '</span>')) AS 'Percentages'
 
 FROM (
-    /* Lead to Tour */
-    (SELECT 'Lead to Tour' AS type
+    /* Lead to Contact */
+    (SELECT 'Lead to Contact' AS type
         , COUNT(DISTINCT IF(DATE(C.creationDtTm) >= '[?From Date]' AND DATE(C.creationDtTm) <= '[?To Date]', C.contactId, NULL)) AS l
-        , COUNT(DISTINCT IF((CT.typeName = '3. Completed Tour' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , COUNT(DISTINCT IF((CT.typeName = '2. Set Tour' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR USR.toUserId IS NOT NULL
                     , C.contactId, NULL)) AS r
-        , 'L to A' AS label
+        , 'Lead to Contact' AS label
         , 0 AS threshold
         , 1 AS Ord
     FROM Contacts C
     LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
     LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '3. Completed Tour'
+                WHERE T.typeName = '2. Set Tour'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') USR
         ON USR.toUserId = C.contactId
 
@@ -29,27 +29,27 @@ FROM (
     AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9')
     )
 
-    UNION /* Tour to Interview */
-    (SELECT 'Tour to Interview' AS type
-        , COUNT(DISTINCT IF((CT.typeName = '3. Completed Tour' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+    UNION /* Contact to Tour */
+    (SELECT 'Contact to Tour' AS type
+        , COUNT(DISTINCT IF((CT.typeName = '2. Set Tour' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR L.toUserId IS NOT NULL
                     , C.contactId, NULL)) AS l
-        , SUM(IF((CT.typeName = '6. Interviewed' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , SUM(IF((CT.typeName = '3. Completed Tour' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR R.toUserId IS NOT NULL
                     , 1, 0)) AS r
-        , 'A to I' AS label
+        , 'Contact to Tour' AS label
         , 0 AS threshold
         , 2 AS Ord
     FROM Contacts C
     LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
     LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '3. Completed Tour'
+                WHERE T.typeName = '2. Set Tour'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') L
         ON L.toUserId = C.contactId
     LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '6. Interviewed'
+                WHERE T.typeName = '3. Completed Tour'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') R
         ON R.toUserId = C.contactId
 
@@ -57,25 +57,25 @@ FROM (
     AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9')
     )
 
-    UNION /* Interview to Application */
-    (SELECT 'Interview to Application' AS type
-        , SUM(IF((CT.typeName = '6. Interviewed' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+    UNION /* Tour to Applied */
+    (SELECT 'Tour to Applied' AS type
+        , SUM(IF((CT.typeName = '3. Completed Tour' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR L.toUserId IS NOT NULL, 1, 0)) AS l
-        , SUM(IF((CT.typeName = '7. Applied' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+        , SUM(IF((CT.typeName = '5. Future Attend Date' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
                 OR R.toUserId IS NOT NULL, 1, 0)) AS r
-        , 'I to APP' AS label
-        , .6 AS threshold
+        , 'Tour to Applied' AS label
+        , 0 AS threshold
         , 3 AS Ord
     FROM Contacts C
     LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
     LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '6. Interviewed'
+                WHERE T.typeName = '3. Completed Tour'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') L
         ON L.toUserId = C.contactId
     LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '7. Applied'
+                WHERE T.typeName = '5. Future Attend Date'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') R
         ON R.toUserId = C.contactId
 
@@ -83,48 +83,27 @@ FROM (
     AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9')
     )
 
-    UNION /* Lead to Application */
-    (SELECT 'Lead to Application' AS type
-        , COUNT(DISTINCT IF(DATE(C.creationDtTm) >= '[?From Date]' AND DATE(C.creationDtTm) <= '[?To Date]', C.contactId, NULL)) AS l
-        , SUM(IF((CT.typeName = '7. Applied' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
-                OR USR.toUserId IS NOT NULL
+    UNION /* Application to Enrolled */
+    (SELECT 'Application to Enrolled' AS type
+        , SUM(IF((CT.typeName = '5. Future Attend Date' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+                OR L.toUserId IS NOT NULL
+                    , 1, 0)) AS l
+        , SUM(IF((CT.typeName = '6. Enrolled Student' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
+                OR R.toUserId IS NOT NULL
                     , 1, 0)) AS r
-        , 'L to APP' AS label
+        , 'Application to Enrolled' AS label
         , 0 AS threshold
         , 4 AS Ord
     FROM Contacts C
     LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
     LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '7. Applied'
-                AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') USR
-        ON USR.toUserId = C.contactId
-
-    WHERE C.isActive = 1
-    AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9')
-    )
-
-    UNION /* Application to Enrollment */
-    (SELECT 'Application to Enrollment' AS type
-        , SUM(IF((CT.typeName = '7. Applied' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
-                OR L.toUserId IS NOT NULL
-                    , 1, 0)) AS l
-        , SUM(IF((CT.typeName = '8. Enrolled' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
-                OR R.toUserId IS NOT NULL
-                    , 1, 0)) AS r
-        , 'APP to E' AS label
-        , .8 AS threshold
-        , 5 AS Ord
-    FROM Contacts C
-    LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
-    LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
-                INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '7. Applied'
+                WHERE T.typeName = '5. Future Attend Date'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') L
         ON L.toUserId = C.contactId
     LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
                 INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '8. Enrolled'
+                WHERE T.typeName = '6. Enrolled Student'
                 AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') R
         ON R.toUserId = C.contactId
 
@@ -132,26 +111,6 @@ FROM (
     AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9')
     )
 
-    UNION /* Lead to Enrollment */
-    (SELECT 'Lead to Enrollment' AS type
-        , COUNT(DISTINCT IF(DATE(C.creationDtTm) >= '[?From Date]' AND DATE(C.creationDtTm) <= '[?To Date]', C.contactId, NULL)) AS l
-        , SUM(IF((CT.typeName = '8. Enrolled' AND DATE(C.lastUpdateDtTm) >= '[?From Date]' AND DATE(C.lastUpdateDtTm) <= '[?To Date]')
-                OR USR.toUserId IS NOT NULL
-                    , 1, 0)) AS r
-        , 'L to E' AS label
-        , .22 AS threshold
-        , 6 AS Ord
-    FROM Contacts C
-    LEFT JOIN ContactTypes CT ON CT.contactTypeId = C.contactTypeId
-    LEFT JOIN (SELECT DISTINCT U.toUserId FROM UserStatusRecords U
-                INNER JOIN ContactTypes T ON T.contactTypeId = U.status
-                WHERE T.typeName = '8. Enrolled'
-                AND DATE(U.updateDtTm) >= '[?From Date]' AND DATE(U.updateDtTm) <= '[?To Date]') USR
-        ON USR.toUserId = C.contactId
-
-    WHERE C.isActive = 1
-    AND SUBSTRING(CT.typeName, 1, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9')
-    )
 ) t1
 
 GROUP BY t1.Ord
